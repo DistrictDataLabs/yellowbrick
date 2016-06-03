cb75e0e26664e74e7264dafcaafcdd340bfba627
Benjamin Bengfort <benjamin@bengfort.com>
2016-06-03 10:58:22 -0700

    basic interface

--- yellowbrick/base.py
+++ yellowbrick/base.py
@@ -13,6 +13,7 @@
 Abstract base classes and interface for Yellowbrick.
 """
 
+from .exceptions import YellowbrickTypeError
 from sklearn.base import BaseEstimator, TransformerMixin
 
 ##########################################################################
@@ -24,18 +25,38 @@ class BaseVisualization(object):
     The root of the visual object hierarchy that defines how yellowbrick
     creates, stores, and renders visual artifcats using matplotlib.
     """
-    pass
+
+    def render(self):
+        """
+        Render is the primary entry point for producing the visualization.
+        """
+        raise NotImplementedError(
+            "All visualizations must specify their own render methodology"
+        )
 
 
 class FeatureVisualization(BaseVisualization, BaseEstimator, TransformerMixin):
     """
-    A feature visualization class accepts as input a DataFrame(s) or Numpy
-    array(s) in order to investigate features individually or together.
+    A feature visualization class accepts as input a DataFrame or Numpy array
+    in order to investigate features individually or together.
 
     FeatureVisualization is itself a transformer so that it can be used in
     a Scikit-Learn Pipeline to perform automatic visual analysis during build.
     """
-    pass
+
+    def fit(self, X, y=None, **kwargs):
+        pass
+
+    def transform(self, X):
+        pass
+
+    def render(self, data=None):
+        """
+        A feature visualization renders data.
+        """
+        raise NotImplementedError(
+            "Please specify how to render the feature visualization"
+        )
 
 
 class ModelVisualization(BaseVisualization, BaseEstimator):
@@ -44,3 +65,17 @@ class ModelVisualization(BaseVisualization, BaseEstimator):
     and is itself an estimator (to be included in a Pipeline) in order to
     visualize the efficacy of a particular fitted model.
     """
+
+    def fit(self, X, y=None, **kwargs):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def render(self, model=None):
+        """
+        A model visualization renders a model
+        """
+        raise NotImplementedError(
+            "Please specify how to render the model visualization"
+        )
---
+++ yellowbrick/exceptions.py
@@ -0,0 +1,45 @@
+# yellowbrick.exceptions
+# Exceptions hierarchy for the yellowbrick library
+#
+# Author:   Benjamin Bengfort <bbengfort@districtdatalabs.com>
+# Created:  Fri Jun 03 10:39:41 2016 -0700
+#
+# Copyright (C) 2016 District Data Labs
+# For license information, see LICENSE.txt
+#
+# ID: exceptions.py [] benjamin@bengfort.com $
+
+"""
+Exceptions hierarchy for the yellowbrick library
+"""
+
+##########################################################################
+## Exceptions Hierarchy
+##########################################################################
+
+class YellowbrickError(Exception):
+    """
+    The root exception for all yellowbrick related errors.
+    """
+    pass
+
+
+class VisualError(YellowbrickError):
+    """
+    A problem when interacting with matplotlib or the display framework.
+    """
+    pass
+
+
+class ModelError(YellowbrickError):
+    """
+    A problem when interacting with sklearn or the ML framework.
+    """
+    pass
+
+
+class YellowbrickTypeError(YellowbrickError, TypeError):
+    """
+    There was an unexpected type or none for a property or input.
+    """
+    pass
