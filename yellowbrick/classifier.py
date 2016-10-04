2a82c4f1fa7c25b346073127115a3e1bd11fde77
Rebecca Bilbro <rebecca.bilbro@bytecubed.com>
2016-10-04 12:07:00 -0400

    preliminary implementation of ClassBalance class

--- yellowbrick/classifier.py
+++ yellowbrick/classifier.py
@@ -25,6 +25,7 @@ from sklearn.metrics import auc, roc_auc_score, roc_curve
 from sklearn.metrics import precision_recall_fscore_support
 
 from .color_utils import ddlheatmap
+from .yb_palettes import YELLOWBRICK_PALETTES
 from .utils import get_model_name, isestimator
 from .base import Visualizer, ScoreVisualizer, MultiModelMixin
 
@@ -166,3 +167,51 @@ class ROCAUC(ClassificationScoreVisualizer):
         plt.ylim([0,1.1])
 
         return plt
+
+##########################################################################
+## Class Balance Chart
+##########################################################################
+
+class ClassBalance(ClassificationScoreVisualizer):
+    """
+    Class balance chart that shows the support for each class in the
+    fitted classification model.
+    """
+    def __init__(self, model, **kwargs):
+        """
+        Pass in a fitted model to generate a class balance chart.
+        """
+        self.estimator = model
+        self.name      = get_model_name(self.estimator)
+        self.colors    = kwargs.pop('colors', YELLOWBRICK_PALETTES['paired'])
+        self.classes   = model.classes_
+
+    def score(self, y, y_pred=None, **kwargs):
+        """
+        Generates the Scikit-Learn precision_recall_fscore_support
+        """
+        self.scores  = precision_recall_fscore_support(y, y_pred, labels=self.classes)
+        self.support = dict(zip(self.classes, self.scores[-1])  )
+        self._draw()
+
+    def _draw(self):
+        """
+        Renders the class balance chart across the axis.
+
+        TODO: Would rather not have to set the colors with this method.
+        Refactor to make better use of yb_palettes module?
+
+        """
+        colors = self.colors[0:len(self.classes)]
+        plt.bar(np.arange(len(self.support)), self.support.values(), color=colors, align='center', width=0.5)
+
+    def poof(self):
+        """
+        Plots a class balance chart
+        """
+        plt.xticks(np.arange(len(self.support)), self.support.keys())
+        cmax, cmin = max(self.support.values()), min(self.support.values())
+        ceiling = cmax + cmax*0.1
+        span = cmax - cmin
+        plt.ylim(0, ceiling)
+        plt.show()
