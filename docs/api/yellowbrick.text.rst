75d9b20b5b927a8fc594e5cc20570de7c41b8c1e
Rebecca Bilbro <rebecca.bilbro@bytecubed.com>
2017-01-21 09:37:01 -0500

    preliminary module and base class for TextVisualizers closes #40

---
+++ docs/api/yellowbrick.text.rst
@@ -0,0 +1,13 @@
+yellowbrick.text package
+========================
+
+Submodules
+----------
+
+yellowbrick.text.base module
+----------------------------
+
+.. automodule:: yellowbrick.text.base
+    :members:
+    :undoc-members:
+    :show-inheritance:
---
+++ yellowbrick/text/__init__.py
@@ -0,0 +1,20 @@
+# yellowbrick.text
+# Visualizers for text feature analysis and diagnostics.
+#
+# Author:   Rebecca Bilbro <rbilbro@districtdatalabs.com>
+# Created:  2017-01-20 14:42
+#
+# Copyright (C) 2017 District Data Labs
+# For license information, see LICENSE.txt
+#
+# ID: __init__.py [] rbilbro@districtdatalabs.com $
+
+"""
+Visualizers for text feature analysis and diagnostics.
+"""
+
+##########################################################################
+## Imports
+##########################################################################
+
+# TODO: add import * statements for textviz modules
