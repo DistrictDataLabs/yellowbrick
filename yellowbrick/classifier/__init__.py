8342e55330627bef2d0228c4e8413a6ccafe3129
NealHumphrey <humphrey.neal@gmail.com>
2017-04-26 20:04:13 -0400

    Refactors ClassificationReport and ConfusionMatrix.
    Refactors tests for above
    Updates ipynb example in nealhumphrey
    
    Includes:
    - partial bug-fix for 3+ class data used w/ the classification report (need to get shape of image right, but no longer errors out)
    - enhances the classificaiton report to use dynamic text color
    
    TODO:
    Need to fix examples.ipynb to import properly (can't do w/out internet)

---
+++ yellowbrick/classifier/__init__.py
@@ -0,0 +1,10 @@
+# yellowbrick.classifier
+# Visualizers for Classification analysis and diagnostics
+
+##########################################################################
+## Imports
+##########################################################################
+
+## Hoist visualizers into the classifier namespace
+from .base import *
+
