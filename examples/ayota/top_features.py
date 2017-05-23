e94cd0aae2f97b29a0594ea0ec6d5b7bbdeed237
Elaine Ayo <ayota@users.noreply.github.com>
2017-05-23 13:53:05 -0400

    added warnings/error handlings for missing class label and feature names

--- examples/ayota/top_features.py
+++ examples/ayota/top_features.py
@@ -14,6 +14,7 @@ TODO:
 ##########################################################################
 
 import numpy as np
+import warnings
 import matplotlib.pyplot as plt
 
 from yellowbrick.classifier import ClassificationScoreVisualizer
@@ -21,7 +22,7 @@ from yellowbrick.exceptions import YellowbrickValueError
 from yellowbrick.style import color_palette
 
 ##########################################################################
-## Top Features Visualizer Quick Function
+## Top Features Visualizer Quick Method
 ##########################################################################
 
 # It would be nice if there were a function here that used the top features
@@ -91,48 +92,13 @@ class TopFeaturesVisualizer(ClassificationScoreVisualizer):
 
     """
 
-    def __init__(self, model, ax=None, N=20, **kwargs):
-        # TODO: Make sure model is a classifier; otherwise throw an error.
+    def __init__(self, model, ax=None, N=20, features=None, **kwargs):
+        # TODO: Make sure model is a classifier; otherwise throw a warning.
 
         super(TopFeaturesVisualizer, self).__init__(model=model, ax=ax, **kwargs)
 
         self.N = N
-        self.model = model
-
-    def _find_top_param(self):
-        """
-        Searches for the parameter on the estimator that contains the array of
-        coefficients or features with the most "importance".
-        
-        Right now, this only checks for the coef_ attribute; feature_importance TK. 
-        If the estimator has neither of these, raise YellowbrickValueError.
-        """
-
-        # NOTE: The order of the search is very important!
-        for attr in ("coef_"):
-            try:
-                return getattr(self.estimator, attr)
-            except AttributeError:
-                continue
-
-        raise YellowbrickValueError(
-            "could not find coef_ param on {} estimator".format(
-                self.estimator.__class__.__name__
-            )
-        )
-
-    def get_top_features(self, class_label, features):
-        """
-        Fetch top N positive and negative coefficients for given class label.
-        """
-
-        values = self._find_top_param()
-        coefs = values[np.where(self.classes_ == class_label)[0][0]]
-        top_positive_coefficients = np.argsort(coefs)[-self.N:]
-        top_negative_coefficients = np.argsort(coefs)[:self.N]
-        top_index = np.hstack([top_negative_coefficients, top_positive_coefficients])
-        self.top_coefficients = coefs[top_index]
-        self.top_features = [features[i] for i in top_index]
+        self.features = features
 
 
     def fit(self, X_train, y, **kwargs):
@@ -140,26 +106,35 @@ class TopFeaturesVisualizer(ClassificationScoreVisualizer):
         Pull in all data transform methods and apply to data here.
         """
 
-        super(TopFeaturesVisualizer, self).fit(X_train, y, **kwargs)
+        self.estimator.fit(X_train, y, **kwargs)
         self.classes_ = self.estimator.classes_
 
         return self
 
-    def score(self, features, class_label, **kwargs):
+    def score(self, class_label=None, **kwargs):
         """
         Extract top N features for desired class label.
         
         Call draw().
         """
 
+        if self.ax is None:
+            self.ax = plt.gca()
+
+        # raise error is no class label provided
+        if class_label is None:
+            raise YellowbrickValueError(
+                "Please provide a class label."
+                )
+
         self.class_label = class_label
-        self.get_top_features(self.estimator, class_label, features)
+        self._get_top_features(class_label=self.class_label, **kwargs)
         self.draw()
 
 
     def draw(self, **kwargs):
         """
-        Draw basic plot here, with all finishing for each individual subplot called here.
+        Draw basic plot.
         """
 
         palette = color_palette(n_colors=2)
@@ -175,3 +150,51 @@ class TopFeaturesVisualizer(ClassificationScoreVisualizer):
 
         self.set_title('Top {N} features for {class_label}'.format(N=self.N, class_label=self.class_label))
         self.ax.set_xticklabels(self.top_features, rotation=90, ha='right')
+
+    def _get_top_param(self):
+        """
+        Searches for the parameter on the estimator that contains the array of
+        coefficients or features with the most "importance."
+
+        Right now, this only checks for the coef_ attribute; feature_importance TK. 
+        If the estimator has neither of these, raise YellowbrickValueError.
+        """
+        try:
+            return getattr(self.estimator, 'coef_')
+        except AttributeError:
+            raise YellowbrickValueError(
+                "Could not find coef_ param on {} estimator. "
+                "Please select estimator with coef_ attribute.".format(
+                    self.estimator.__class__.__name__
+                )
+            )
+
+    def _get_top_features(self, class_label=None, features=None):
+        """
+        Fetch top N positive and negative coefficients for given class label.
+
+        Also fetches corresponding names of features if they exist; otherwise
+        assign index numbers and throw a warning.
+        
+        Throws warning if no class label defined.
+        """
+        # check for user-provided feature in visualizer init
+        if self.features:
+            features = self.features
+
+        # fetch top feature values, get values for desired class label
+        values = self._get_top_param()
+        coefs = values[np.where(self.classes_ == class_label)[0][0]]
+        top_positive_coefficients = np.argsort(coefs)[-self.N:]
+        top_negative_coefficients = np.argsort(coefs)[:self.N]
+        top_index = np.hstack([top_negative_coefficients, top_positive_coefficients])
+        self.top_coefficients = coefs[top_index]
+
+        # get pretty names if they exist
+        try:
+            self.top_features = [features[i] for i in top_index]
+        except TypeError:
+            self.top_features = top_index
+            warnings.warn(
+                "No feature names specified. Index values will be used."
+            )
