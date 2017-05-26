384918f7bf7123c6c9ec140def6cf9b9342644f2
Benjamin Bengfort <benjamin@bengfort.com>
2017-05-26 13:56:30 -0400

    New and improved ROCAUC curves as well as code clean up Micro and macro averages and per-class curves closes #153 Image based testing for ROCAUC

--- yellowbrick/style/palettes.py
+++ yellowbrick/style/palettes.py
@@ -503,7 +503,6 @@ def color_palette(palette=None, n_colors=None):
             n_colors = len(palette)
 
     elif not isinstance(palette, string_types):
-        palette = palette
         if n_colors is None:
             n_colors = len(palette)
 
