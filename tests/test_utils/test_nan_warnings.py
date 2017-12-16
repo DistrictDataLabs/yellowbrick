93a2a05ebf9065a1bef3b6674503521038f5c60f
Taylor Miller <taylor.miller@healthcatalyst.com>
2017-12-16 15:29:11 -0700

    * forgot to stage the updated tests that now test the higher level clean_data function

--- tests/test_utils/test_nan_warnings.py
+++ tests/test_utils/test_nan_warnings.py
@@ -1,12 +1,11 @@
 """Test the RadViz feature analysis visualizers."""
 
-import pytest
 import numpy as np
+import pytest
 
-from yellowbrick.utils.nan_warnings import warn_if_nans_exist, \
-    count_rows_with_nans, \
-    drop_rows_containing_nans, count_nan_elements
 from yellowbrick.exceptions import DataWarning
+from yellowbrick.utils.nan_warnings import count_nan_elements, \
+    count_rows_with_nans, warn_if_nans_exist, clean_data
 
 
 def test_raise_warning_if_nans_exist():
@@ -56,21 +55,21 @@ def test_count_nan_elements():
     assert count_nan_elements(data3) == 3
 
 
-def test_drop_nan_rows_no_nans():
+def test_clean_data_X_only_no_nans():
     """Test that an array with no nulls is returned intact."""
-    data = np.array([
+    X = np.array([
         [1, 2, 3],
         [4, 5, 6],
         [7, 8, 9],
     ])
 
-    observed = drop_rows_containing_nans(data)
-    np.testing.assert_array_equal(data, observed)
+    observed = clean_data(X)
+    np.testing.assert_array_equal(X, observed)
 
 
-def test_drop_nan_rows():
+def test_clean_data_X_only():
     """Test that arrays with missing data are returned without nans."""
-    data = np.array([
+    X = np.array([
         [1, 2, np.nan],
         [4, 5, 6],
         [np.nan, np.nan, np.nan],
@@ -79,6 +78,68 @@ def test_drop_nan_rows():
     expected = np.array([
         [4, 5, 6]
     ])
+    observed = clean_data(X)
 
-    observed = drop_rows_containing_nans(data)
     np.testing.assert_array_equal(expected, observed)
+
+
+def test_clean_data_dirty_X_dirty_y():
+    """Test that arrays with missing data are returned without nans."""
+    X = np.array([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, np.nan],
+        [np.nan, np.nan, np.nan],
+    ])
+    y = np.array([33, np.nan, 44, np.nan])
+
+    expected_X = np.array([
+        [1, 2, 3],
+    ])
+    expected_y = np.array([33])
+    observed_X, observed_y = clean_data(X, y)
+
+    np.testing.assert_array_equal(expected_X, observed_X)
+    np.testing.assert_array_equal(expected_y, observed_y)
+
+
+def test_clean_data_dirty_X_clean_y():
+    """Test that arrays with missing data are returned without nans."""
+    X = np.array([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, np.nan],
+        [np.nan, np.nan, np.nan],
+    ])
+    y = np.array([33, 44, 55, 66])
+
+    expected_X = np.array([
+        [1, 2, 3],
+        [4, 5, 6],
+    ])
+    expected_y = np.array([33, 44])
+    observed_X, observed_y = clean_data(X, y)
+
+    np.testing.assert_array_equal(expected_X, observed_X)
+    np.testing.assert_array_equal(expected_y, observed_y)
+
+
+def test_clean_data_clean_X_dirty_y():
+    """Test that arrays with missing data are returned without nans."""
+    X = np.array([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [10, 11, 12]
+    ])
+    y = np.array([np.nan, 44, np.nan, 66])
+
+    expected_X = np.array([
+        [4, 5, 6],
+        [10, 11, 12]
+    ])
+    expected_y = np.array([44, 66])
+    observed_X, observed_y = clean_data(X, y)
+
+    np.testing.assert_array_equal(expected_X, observed_X)
+    np.testing.assert_array_equal(expected_y, observed_y)
