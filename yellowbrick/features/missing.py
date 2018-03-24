9d3d69ff7b595d28aa33bb8ab6b345f8a305034f
Nathan Danielsen <nathan.danielsen@gmail.com>
2018-03-24 09:49:20 -0400

    add example notebook in progress

--- yellowbrick/features/missing.py
+++ yellowbrick/features/missing.py
@@ -27,8 +27,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from yellowbrick.utils import is_dataframe
+from yellowbrick.utils import is_structured_array
 from yellowbrick.features.base import DataVisualizer
-from yellowbrick.exceptions import YellowbrickTypeError, NotFitted
+
+# from yellowbrick.style.colors import resolve_colors
+
 
 
 ##########################################################################
@@ -51,53 +54,43 @@ class MissingValuesBarVisualizer(DataVisualizer):
                  **kwargs):
         """
         """
-        super(ScatterVisualizer, self).__init__(ax, features, classes, color,
+
+        super(MissingValuesBarVisualizer, self).__init__(ax, features, classes, color,
                                                 colormap, **kwargs)
-
-        self.x = x
-        self.y = y
-
-        self.color = color
-        self.colormap = colormap
-
-        if self.x is not None and self.y is not None and self.features_ is not None:
-            raise YellowbrickValueError(
-                'Please specify x,y or features, not both.')
-
-        if self.x is not None and self.y is not None and self.features_ is None:
-            self.features_ = [self.x, self.y]
-
 
 
     def fit(self, X, y=None, **kwargs):
         """
+        TODO if y, then color code the missing values in the chart?
+
 
         """
+        nrows, ncols = df.shape
 
         # Handle the feature names if they're None.
         if self.features_ is not None and is_dataframe(X):
-            X_two_cols = X[self.features_].as_matrix()
+            X = X[self.features_].as_matrix()
 
         # handle numpy named/ structured array
         elif self.features_ is not None and is_structured_array(X):
             X_selected = X[self.features_]
-            X_two_cols = X_selected.copy().view((np.float64, len(X_selected.dtype.names)))
-
-        # handle features that are numeric columns in ndarray matrix
-        elif self.features_ is not None and has_ndarray_int_columns(self.features_, X):
-            f_one, f_two = self.features_
-            X_two_cols = X[:, [int(f_one), int(f_two)]]
+            X = X_selected.copy().view((np.float64, len(X_selected.dtype.names)))
 
         else:
             pass
 
-        # Store the classes for the legend if they're None.
+        if self.features_ is None:
+            self.features_ = range(nrows)
+
         if self.classes_ is None:
             # TODO: Is this the most efficient method?
             self.classes_ = [str(label) for label in np.unique(y)]
 
+        nan_matrix = X.astype(float)
+        self.nan_col_counts = [np.count_nonzero(np.isnan(col)) for col in nan_matrix.T]
+
         # Draw the instances
-        self.draw(X_two_cols, y, **kwargs)
+        self.draw(X, y, **kwargs)
 
         # Fit always returns self.
         return self
@@ -107,3 +100,30 @@ class MissingValuesBarVisualizer(DataVisualizer):
         draws each instance as a class or target colored point, whose location
         is determined by the feature data set.
         """
+
+        width = 0.5  # the width of the bars
+
+        self.ax.bar(ind - width/2, self.nan_col_counts, width,
+                        color='black')
+
+    def finalize(self, **kwargs):
+        """
+        Finalize executes any subclass-specific axes finalization steps.
+        The user calls poof and poof calls finalize.
+
+        Parameters
+        ----------
+        kwargs: generic keyword arguments.
+
+        """
+        # Set the title
+        self.set_title(
+            'Missing Values by Column'
+        )
+        ind = np.arange(len(self.features_))  # the x locations for the groups
+        # Remove the ticks from the graph
+        self.ax.set_ylabel('Count')
+        self.ax.set_xticks(ind)
+        self.ax.set_xticklabels(self.features_, rotation='vertical')
+        # Add the legend
+        self.ax.legend(loc='best')
