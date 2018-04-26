6e010026a9f334f856548b92ec5605b67832cbf5
Nathan Danielsen <nathan.danielsen@gmail.com>
2018-04-25 21:15:39 -0400

    pandas working with missing

--- tests/test_missing/test_dispersion.py
+++ tests/test_missing/test_dispersion.py
@@ -16,3 +16,61 @@ Tests for the MissingValuesDispersion visualizations.
 ##########################################################################
 ## Imports
 ##########################################################################
+from sklearn.datasets import make_classification
+
+from tests.base import VisualTestCase
+from yellowbrick.missing.dispersion import *
+
+try:
+    import pandas as pd
+except ImportError:
+    pd = None
+
+##########################################################################
+## Feature Importances Tests
+##########################################################################
+
+class MissingValuesDispersionTestCase(VisualTestCase):
+    """
+    MissingValuesDispersion visualizer
+    """
+
+    def test_missingvaluesdispersion(self):
+        """
+        Integration test of visualizer with feature importances param
+        """
+        X, y = make_classification(
+            n_samples=400, n_features=20, n_informative=8, n_redundant=8,
+            n_classes=2, n_clusters_per_class=4, random_state=854
+        )
+
+        # add nan values to a range of values in the matrix
+        X[X > 1.5] = np.nan
+
+        features = [str(n) for n in range(20)]
+        viz = MissingValuesDispersion(features=features)
+        viz.fit(X, y=y)
+        viz.poof()
+
+        self.assert_images_similar(viz)
+
+
+    def test_missingvaluesdispersion_with_pandas(self):
+        """
+        Integration test of visualizer with feature importances param
+        """
+        X, y = make_classification(
+            n_samples=400, n_features=20, n_informative=8, n_redundant=8,
+            n_classes=2, n_clusters_per_class=4, random_state=854
+        )
+
+        # add nan values to a range of values in the matrix
+        X[X > 1.5] = np.nan
+
+        X_ = pd.DataFrame(X)
+        features = [str(n) for n in range(20)]
+        viz = MissingValuesDispersion(features=features)
+        viz.fit(X_, y=y)
+        viz.poof()
+
+        self.assert_images_similar(viz)
