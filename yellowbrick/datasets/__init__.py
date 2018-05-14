e15c9ad90f6046f4a0b430799ef178f86ecb2bf4
RaulPL <raulpl25@gmail.com>
2018-05-14 15:13:06 -0500

    Adding functions to load the available datasets. Similar to sklearn

---
+++ yellowbrick/datasets/__init__.py
@@ -0,0 +1,23 @@
+from .download import load_concrete
+from .download import load_energy
+from .download import load_credit
+from .download import load_occupancy
+from .download import load_mushroom
+from .download import load_hobbies
+from .download import load_game
+from .download import load_bikeshare
+from .download import load_spam
+
+
+
+
+
+
+
+
+
+
+
+
+
+
