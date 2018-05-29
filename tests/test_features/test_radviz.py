dfdbe3e8f0dd91ee6d824bc454e55dd103e7c5e5
Benjamin Bengfort <benjamin@bengfort.com>
2018-05-29 18:11:52 -0400

    radviz alpha documentation and improved tests

--- tests/test_features/test_radviz.py
+++ tests/test_features/test_radviz.py
@@ -19,11 +19,12 @@ Test the RadViz feature analysis visualizers
 
 import sys
 import pytest
-import unittest
 import numpy.testing as npt
 
 from tests.base import VisualTestCase
-from tests.dataset import DatasetMixin
+from tests.dataset import DatasetMixin, Dataset
+from sklearn.datasets import make_classification
+
 from yellowbrick.features.radviz import *
 
 try:
@@ -31,13 +32,42 @@ try:
 except ImportError:
     pandas = None
 
+
 ##########################################################################
-## RadViz Base Tests
+## Fixtures
 ##########################################################################
 
+@pytest.fixture(scope='class')
+def dataset(request):
+    """
+    Creates a random multiclass classification dataset fixture
+    """
+    X, y = make_classification(
+        n_samples=200, n_features=5, n_informative=4, n_redundant=0,
+        n_classes=3, n_clusters_per_class=1, random_state=451, flip_y=0,
+        class_sep=3, scale=np.array([1.0, 2.0, 100.0, 20.0, 1.0])
+    )
 
-class RadVizTests(VisualTestCase, DatasetMixin):
-    X = np.array(
+    dataset = Dataset(X, y)
+    request.cls.dataset = dataset
+
+
+##########################################################################
+## RadViz Tests
+##########################################################################
+
+@pytest.mark.usefixtures('dataset')
+class TestRadViz(VisualTestCase, DatasetMixin):
+    """
+    Test the RadViz visualizer
+    """
+
+    def test_normalize_x(self):
+        """
+        Test the static normalization method on the RadViz class
+        """
+        # Original data
+        X = np.array(
             [[ 2.318, 2.727, 4.260, 7.212, 4.792],
              [ 2.315, 2.726, 4.295, 7.140, 4.783,],
              [ 2.315, 2.724, 4.260, 7.135, 4.779,],
@@ -46,21 +76,8 @@ class RadVizTests(VisualTestCase, DatasetMixin):
              [ 2.110, 3.620, 4.470, 8.210, 5.612,]]
         )
 
-    y = np.array([1, 1, 0, 1, 0, 0])
-
-    def setUp(self):
-        self.occupancy = self.load_data('occupancy')
-        super(RadVizTests, self).setUp()
-
-    def tearDown(self):
-        self.occupancy = None
-        super(RadVizTests, self).tearDown()
-
-    def test_normalize_x(self):
-        """
-        Test the static normalization method on the RadViz class
-        """
-        expected = np.array(
+        # Expected result
+        Xe = np.array(
             [[ 1.        ,  0.00332594,  0.        ,  0.07162791,  0.01543943],
              [ 0.98557692,  0.00221729,  0.16666667,  0.00465116,  0.00475059],
              [ 0.98557692,  0.        ,  0.        ,  0.        ,  0.        ],
@@ -69,88 +86,66 @@ class RadVizTests(VisualTestCase, DatasetMixin):
              [ 0.        ,  0.99334812,  1.        ,  1.        ,  0.98931116]]
         )
 
-        Xp = RadViz.normalize(self.X)
-        npt.assert_array_almost_equal(Xp, expected)
+        # Xprime (transformed X)
+        Xp = RadViz.normalize(X)
+        npt.assert_array_almost_equal(Xp, Xe)
 
     def test_radviz(self):
         """
-        Assert no errors occur during radviz visualizer integration
+        Assert image similarity on test dataset
         """
         visualizer = RadViz()
-        visualizer.fit_transform(self.X, self.y)
+        visualizer.fit_transform(self.dataset.X, self.dataset.y)
         visualizer.poof()
         self.assert_images_similar(visualizer, tol=0.25)
 
-    def test_integrated_radviz(self):
+    def test_radviz_alpha(self):
         """
-        Test radviz on the real, occupancy data set
+        Assert image similarity with alpha transparency
         """
-
-        # Load the data from the fixture
-        X = self.occupancy[[
-            "temperature", "relative_humidity", "light", "C02", "humidity"
-        ]]
-        X = X.copy().view((float, len(X.dtype.names)))
-        y = self.occupancy['occupancy'].astype(int)
-
-        # Test the visualizer
-        visualizer = RadViz()
-        visualizer.fit_transform(X, y)
+        visualizer = RadViz(alpha=0.5)
+        visualizer.fit_transform(self.dataset.X, self.dataset.y)
         visualizer.poof()
         self.assert_images_similar(visualizer, tol=0.25)
 
     @pytest.mark.xfail(
         sys.platform == 'win32', reason="images not close on windows"
     )
-    @unittest.skipUnless(pandas is not None,
-                         "Pandas is not installed, could not run test.")
+    @pytest.mark.skipif(pandas is None, reason="test requires Pandas")
     def test_integrated_radiz_with_pandas(self):
         """
-        Test scatterviz on the real, occupancy data set with pandas
+        Test RadViz with Pandas on the occupancy dataset
         """
-        # Load the data from the fixture
-        X = self.occupancy[[
-            "temperature", "relative_humidity", "light", "C02", "humidity"
-        ]]
-        y = self.occupancy['occupancy'].astype(int)
+        occupancy = self.load_pandas("occupancy")
 
-        # Convert X to a pandas dataframe
-        X = pandas.DataFrame(X)
-        X.columns = [
-            "temperature", "relative_humidity", "light", "C02", "humidity"
-        ]
+        # Load the data from the fixture
+        X = occupancy[[
+            "temperature", "relative humidity", "light", "C02", "humidity"
+        ]]
+        y = occupancy['occupancy'].astype(int)
 
         # Test the visualizer
-        features = ["temperature", "relative_humidity"]
-        visualizer = RadViz(features=features)
+        visualizer = RadViz()
         visualizer.fit_transform_poof(X, y)
         self.assert_images_similar(visualizer)
 
     @pytest.mark.xfail(
         sys.platform == 'win32', reason="images not close on windows"
     )
-    @unittest.skipUnless(pandas is not None,
-                         "Pandas is not installed, could not run test.")
-    def test_integrated_radiz_with_pandas_with_classes(self):
+    @pytest.mark.skipif(pandas is None, reason="test requires Pandas")
+    def test_integrated_radiz_pandas_classes_features(self):
         """
-        Test scatterviz on the real, occupancy data set with pandas with classes
+        Test RadViz with classes and features specified
         """
         # Load the data from the fixture
-        X = self.occupancy[[
-            "temperature", "relative_humidity", "light", "C02", "humidity"
-        ]]
+        occupancy = self.load_pandas("occupancy")
+        features = ["temperature", "relative humidity", "light"]
         classes = ['unoccupied', 'occupied']
 
-        y = self.occupancy['occupancy'].astype(int)
-
-        # Convert X to a pandas dataframe
-        X = pandas.DataFrame(X)
-        X.columns = [
-            "temperature", "relative_humidity", "light", "C02", "humidity"
-        ]
+        X = occupancy[features]
+        y = occupancy['occupancy'].astype(int)
 
         # Test the visualizer
-        features = ["temperature", "relative_humidity"]
         visualizer = RadViz(features=features, classes=classes)
         visualizer.fit_transform_poof(X, y)
         self.assert_images_similar(visualizer)
