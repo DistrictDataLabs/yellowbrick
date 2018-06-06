f843c4471b5dac100cc837f69504a45570370a86
Jason Le <le.kent.jason@gmail.com>
2018-06-06 06:34:17 -0400

    removed visualizer tests in type checking (#410)
    Removed the redundant type check to see if the estimator was a visualizer, tests still pass without requiring any additional changes. 
    
    Fixes #367

--- yellowbrick/utils/types.py
+++ yellowbrick/utils/types.py
@@ -61,12 +61,6 @@ def is_classifier(estimator):
     is_classifier
         `sklearn.is_classifier() <https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/base.py#L518>`_
     """
-    # TODO: once we make ScoreVisualizer and ModelVisualizer pass through
-    # wrappers as in Issue #90, these three lines become unnecessary.
-    # NOTE: This must be imported here to avoid recursive import.
-    from yellowbrick.base import Visualizer
-    if isinstance(estimator, Visualizer):
-        return is_classifier(estimator.estimator)
 
     # Test the _estimator_type property
     return getattr(estimator, "_estimator_type", None) == "classifier"
@@ -90,12 +84,6 @@ def is_regressor(estimator):
     is_regressor
         `sklearn.is_regressor() <https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/base.py#L531>`_
     """
-    # TODO: once we make ScoreVisualizer and ModelVisualizer pass through
-    # wrappers as in Issue #90, these three lines become unnecessary.
-    # NOTE: This must be imported here to avoid recursive import.
-    from yellowbrick.base import Visualizer
-    if isinstance(estimator, Visualizer):
-        return is_regressor(estimator.estimator)
 
     # Test the _estimator_type property
     return getattr(estimator, "_estimator_type", None) == "regressor"
@@ -114,12 +102,6 @@ def is_clusterer(estimator):
         The object to test if it is a Scikit-Learn clusterer, especially a
         Scikit-Learn estimator or Yellowbrick visualizer
     """
-    # TODO: once we make ScoreVisualizer and ModelVisualizer pass through
-    # wrappers as in Issue #90, these three lines become unnecessary.
-    # NOTE: This must be imported here to avoid recursive import.
-    from yellowbrick.base import Visualizer
-    if isinstance(estimator, Visualizer):
-        return is_clusterer(estimator.estimator)
 
     # Test the _estimator_type property
     return getattr(estimator, "_estimator_type", None) == "clusterer"
@@ -138,12 +120,6 @@ def is_gridsearch(estimator):
         The object to test if it is a Scikit-Learn clusterer, especially a
         Scikit-Learn estimator or Yellowbrick visualizer
     """
-    # TODO: once we make ScoreVisualizer and ModelVisualizer pass through
-    # wrappers as in Issue #90, these three lines become unnecessary.
-    # NOTE: This must be imported here to avoid recursive import.
-    from yellowbrick.base import Visualizer
-    if isinstance(estimator, Visualizer):
-        return is_gridsearch(estimator.estimator)
 
     from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
