6512ab31baa623e36ddd765974384860ec79058c
Benjamin Bengfort <benjamin@bengfort.com>
2018-06-18 12:47:11 -0400

    PCADecomposition test enhancements
    Added image similarity tests to all facets of PCADecomposition testing
    as well as image similarity tests for biplot PCA visualizations.

--- yellowbrick/features/pca.py
+++ yellowbrick/features/pca.py
@@ -2,10 +2,9 @@
 # Decomposition based feature visualization with PCA.
 #
 # Author:   Carlo Morales <@cjmorale>
+# Author:   Raúl Peralta Lozada <@RaulPL>
+# Author:   Benjamin Bengfort <@bbengfort>
 # Created:  Tue May 23 18:34:27 2017 -0400
-#
-# Copyright (C) 2017 District Data Labs
-# For license information, see LICENSE.txt
 #
 # ID: pca.py [] cmorales@pacificmetrics.com $
 
@@ -31,87 +30,26 @@ from sklearn.preprocessing import StandardScaler
 
 
 ##########################################################################
-## Quick Methods
-##########################################################################
-
-def pca_decomposition(X, y=None, ax=None, scale=True, proj_dim=2,
-                      colormap=palettes.DEFAULT_SEQUENCE, color=None, **kwargs):
-    """Produce a two or three dimensional principal component plot of the data array ``X``
-    projected onto it's largest sequential principal components. It is common practice to scale the
-    data array ``X`` before applying a PC decomposition. Variable scaling can be controlled using
-    the ``scale`` argument.
-
-    Parameters
-    ----------
-    X : ndarray or DataFrame of shape n x m
-        A matrix of n instances with m features.
-
-    y : ndarray or Series of length n
-        An array or series of target or class values.
-
-    ax : matplotlib Axes, default: None
-        The axes to plot the figure on.
-
-    scale : bool, default: True
-        Boolean that indicates if the values of X should be scaled.
-
-    proj_dim : int, default: 2
-        Dimension of the PCA visualizer.
-
-    colormap : string or cmap, default: None
-        Optional string or matplotlib cmap to colorize lines.
-        Use either color to colorize the lines on a per class basis or
-        colormap to color them on a continuous scale.
-
-    color : list or tuple of colors, default: None
-        Specify the colors for each individual class.
-
-    kwargs : dict
-        Keyword arguments that are passed to the base class and may influence
-        the visualization as defined in other Visualizers.
-
-    Examples
-    --------
-    >>> from sklearn import datasets
-    >>> iris = datasets.load_iris()
-    >>> X = iris.data
-    >>> y = iris.target
-    >>> pca_decomposition(X, color=y, proj_dim=3, colormap='RdBu_r')
-
-    """
-    # Instantiate the visualizer
-    visualizer = PCADecomposition(X=X, y=y, ax=ax, scale=scale, proj_dim=proj_dim,
-                                  colormap=colormap, color=color)
-
-    # Fit and transform the visualizer (calls draw)
-    visualizer.fit(X, y, **kwargs)
-    visualizer.transform(X)
-
-    # Return the axes object on the visualizer
-    return visualizer.poof()
-
-##########################################################################
 ##2D and #3D PCA Visualizer
 ##########################################################################
 
 class PCADecomposition(MultiFeatureVisualizer):
     """
-    Produce a two or three dimensional principal component plot of the data array ``X``
-    projected onto it's largest sequential principal components. It is common practice to scale the
-    data array ``X`` before applying a PC decomposition. Variable scaling can be controlled using
-    the ``scale`` argument.
+    Produce a two or three dimensional principal component plot of a data array
+    projected onto it's largest sequential principal components. It is common
+    practice to scale the data array ``X`` before applying a PC decomposition.
+    Variable scaling can be controlled using the ``scale`` argument.
 
     Parameters
     ----------
-    X : ndarray or DataFrame of shape n x m
-        A matrix of n instances with m features.
-
-    y : ndarray or Series of length n
-        An array or series of target or class values.
-
     ax : matplotlib Axes, default: None
         The axes to plot the figure on. If None is passed in the current axes.
         will be used (or generated if required).
+
+    features: list, default: None
+        a list of feature names to use
+        If a DataFrame is passed to fit and features is None, feature
+        names are selected as the columns of the DataFrame.
 
     scale : bool, default: True
         Boolean that indicates if user wants to scale data.
@@ -119,6 +57,10 @@ class PCADecomposition(MultiFeatureVisualizer):
     proj_dim : int, default: 2
         Dimension of the PCA visualizer.
 
+    proj_features : bool, default: False
+        Boolean that indicates if the user wants to project the features
+        in the projected space. If True the plot will be similar to a biplot.
+
     color : list or tuple of colors, default: None
         Specify the colors for each individual class.
 
@@ -127,9 +69,11 @@ class PCADecomposition(MultiFeatureVisualizer):
         Use either color to colorize the lines on a per class basis or
         colormap to color them on a continuous scale.
 
-    proj_features : bool, default: False
-        Boolean that indicates if the user wants to project the features
-        in the projected space. If True the plot will be similar to a biplot.
+    random_state : int, RandomState instance or None, optional (default None)
+        If input data is larger than 500x500 and the number of components to
+        extract is lower than 80% of the smallest dimension of the data, then
+        the more efficient `randomized` solver is enabled, this parameter sets
+        the random state on this solver.
 
     kwargs : dict
         Keyword arguments that are passed to the base class and may influence
@@ -148,27 +92,56 @@ class PCADecomposition(MultiFeatureVisualizer):
     >>> visualizer.poof()
 
     """
-    def __init__(self, features=None, ax=None, scale=True, color=None,
-                 proj_dim=2, colormap=palettes.DEFAULT_SEQUENCE,
-                 proj_features=False, **kwargs):
+    def __init__(self,
+                 ax=None,
+                 features=None,
+                 scale=True,
+                 proj_dim=2,
+                 proj_features=False,
+                 color=None,
+                 colormap=palettes.DEFAULT_SEQUENCE,
+                 random_state=None,
+                 **kwargs):
         super(PCADecomposition, self).__init__(ax=ax,
                                                features=features,
                                                **kwargs)
-        # Data Parameters
+
         if proj_dim not in (2, 3):
             raise YellowbrickValueError("proj_dim object is not 2 or 3.")
-        self.color = color
+
+        # Data Parameters
         self.scale = scale
         self.proj_dim = proj_dim
+        self.proj_features = proj_features
+
+        # Create the PCA transformer
         self.pca_transformer = Pipeline(
             [('scale', StandardScaler(with_std=self.scale)),
-             ('pca', PCA(self.proj_dim, ))]
+             ('pca', PCA(self.proj_dim, random_state=random_state))]
         )
-        self.proj_features = proj_features
+
         # Visual Parameters
+        self.color = color
         self.colormap = colormap
 
     def fit(self, X, y=None, **kwargs):
+        """
+        Fits the PCA transformer, transforms the data in X, then draws the
+        decomposition in either 2D or 3D space as a scatter plot.
+
+        Parameters
+        ----------
+        X : ndarray or DataFrame of shape n x m
+            A matrix of n instances with m features.
+
+        y : ndarray or Series of length n
+            An array or series of target or class values.
+
+        Returns
+        -------
+        self : visualizer
+            Returns self for use in Pipelines
+        """
         super(PCADecomposition, self).fit(X=X, y=y, **kwargs)
         self.pca_transformer.fit(X)
         self.pca_components_ = self.pca_transformer.steps[1][1].components_
@@ -235,3 +208,86 @@ class PCADecomposition(MultiFeatureVisualizer):
         self.ax.set_ylabel('Principal Component 2')
         if self.proj_dim == 3:
             self.ax.set_zlabel('Principal Component 3')
+
+
+##########################################################################
+## Quick Method
+##########################################################################
+
+def pca_decomposition(X, y=None, ax=None, features=None, scale=True,
+                      proj_dim=2, proj_features=False, color=None,
+                      colormap=palettes.DEFAULT_SEQUENCE,
+                      random_state=None, **kwargs):
+    """Produce a two or three dimensional principal component plot of the data array ``X``
+    projected onto it's largest sequential principal components. It is common practice to scale the
+    data array ``X`` before applying a PC decomposition. Variable scaling can be controlled using
+    the ``scale`` argument.
+
+    Parameters
+    ----------
+    X : ndarray or DataFrame of shape n x m
+        A matrix of n instances with m features.
+
+    y : ndarray or Series of length n
+        An array or series of target or class values.
+
+    ax : matplotlib Axes, default: None
+        The axes to plot the figure on. If None is passed in the current axes.
+        will be used (or generated if required).
+
+    features: list, default: None
+        a list of feature names to use
+        If a DataFrame is passed to fit and features is None, feature
+        names are selected as the columns of the DataFrame.
+
+    scale : bool, default: True
+        Boolean that indicates if user wants to scale data.
+
+    proj_dim : int, default: 2
+        Dimension of the PCA visualizer.
+
+    proj_features : bool, default: False
+        Boolean that indicates if the user wants to project the features
+        in the projected space. If True the plot will be similar to a biplot.
+
+    color : list or tuple of colors, default: None
+        Specify the colors for each individual class.
+
+    colormap : string or cmap, default: None
+        Optional string or matplotlib cmap to colorize lines.
+        Use either color to colorize the lines on a per class basis or
+        colormap to color them on a continuous scale.
+
+    random_state : int, RandomState instance or None, optional (default None)
+        If input data is larger than 500x500 and the number of components to
+        extract is lower than 80% of the smallest dimension of the data, then
+        the more efficient `randomized` solver is enabled, this parameter sets
+        the random state on this solver.
+
+    kwargs : dict
+        Keyword arguments that are passed to the base class and may influence
+        the visualization as defined in other Visualizers.
+
+    Examples
+    --------
+    >>> from sklearn import datasets
+    >>> iris = datasets.load_iris()
+    >>> X = iris.data
+    >>> y = iris.target
+    >>> pca_decomposition(X, color=y, proj_dim=3, colormap='RdBu_r')
+
+    """
+    # Instantiate the visualizer
+    visualizer = PCADecomposition(
+        ax=ax, features=features, scale=scale, proj_dim=proj_dim,
+        proj_features=proj_features, color=color, colormap=colormap,
+        random_state=random_state, **kwargs
+    )
+
+    # Fit and transform the visualizer (calls draw)
+    visualizer.fit(X, y)
+    visualizer.transform(X)
+    visualizer.poof()
+
+    # Return the axes object on the visualizer
+    return visualizer.ax
