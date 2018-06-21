25adeaff8bedcc1748510290edcb7ed6e61ffaaf
Larry Gray <lwgray@ip-172-26-12-110.us-east-2.compute.internal>
2018-06-21 23:22:08 +0000

    Fix #147 Implements a lexical disperision plot
    Builds DispersionPlotVisualizer that shows the occurence of a word in a
    corpus and the distance each word is from the beginning of the corpus.

---
+++ yellowbrick/text/dispersion.py
@@ -0,0 +1,119 @@
+# yellowbrick.text.dispersion
+# Implementations of lexical dispersions for text visualization.
+#
+# Author:   Larry Gray
+# Created:  2018-06-21 10:06
+#
+# Copyright (C) 2018 District Data Labs
+# For license information, see LICENSE.txt
+#
+# ID: dispersion.py [67b2740] lwgray@gmail.com $
+
+"""
+Implementation of lexical dispersion for text visualization
+"""
+
+
+##########################################################################
+## Imports
+##########################################################################
+
+import numpy as np
+from yellowbrick.text.base import TextVisualizer
+from yellowbrick.exceptions import YellowbrickValueError
+
+##########################################################################
+## Dispersion Plot Visualizer
+##########################################################################
+
+
+class DispersionPlotVisualizer(TextVisualizer):
+    """
+    DispersionPlotVisualizer allows for visualization of the lexical dispersion
+    of words in a corpus.  Lexical dispersion is a measure of a word's
+    homeogeneity across the parts of a corpus.  This plot notes the occurences
+    of a word and how many words from the beginning it appears.
+    
+    Parameters
+    ----------
+    words : list, default: None
+        The list of words to examine their dispersion
+    
+    ax : matplotlib axes, default: None
+        The axes to plot the figure on.
+    
+    color : list or tuple of colors
+        Specify color for bars
+        
+    kwargs : dict
+        Pass any additional keyword arguments to the super class.
+    
+    These parameters can be influenced later on in the visualization
+    process, but can and should be set as early as possible.
+    """
+    
+    def __init__(self, words, ax=None, color=None, ignore_case=False, **kwargs):
+        super(DispersionPlotVisualizer, self).__init__(ax=ax, **kwargs)
+        
+        self.color = color
+        self.words = words
+        self.ignore_case = ignore_case
+        
+    def fit(self, X):
+        """
+        The fit method is the primary drawing input for the dispersion
+        visualization. It requires the corpus as a list of words.
+        
+        Parameters
+        ----------
+        X : List
+            A list of words in the order they appear in the corpus.
+        """
+            
+        text = list(X)
+        self.words.reverse()
+
+        if self.ignore_case:
+            words_to_comp = list(map(str.lower, self.words))
+            text_to_comp = list(map(str.lower, text))
+        else:
+            words_to_comp = self.words
+            text_to_comp = text
+
+        points = [(x,y) for x in range(len(text_to_comp))
+                        for y in range(len(words_to_comp))
+                        if text_to_comp[x] == words_to_comp[y]]
+        if points:
+            self.x_, self.y_ = list(zip(*points))
+        else:
+            self.x_ = self.y_ = ()
+        
+        self.draw()
+        return self
+    
+    def draw(self, **kwargs):
+        """
+        Called from the fit method, this method creates the canvas and
+        draws the distribution plot on it.
+        Parameters
+        ----------
+        kwargs: generic keyword arguments.
+        """
+        
+        self.ax.scatter(self.x_, self.y_, marker='|', color=self.color)
+        self.ax.set_yticks(list(range(len(self.words))))
+        self.ax.set_yticklabels(self.words)
+        
+    def finalize(self, **kwargs):
+        """
+        The finalize method executes any subclass-specific axes
+        finalization steps. The user calls poof & poof calls finalize.
+        Parameters
+        ----------
+        kwargs: generic keyword arguments.
+        """
+        
+        self.ax.set_ylim(-1, len(self.words))
+        self.ax.set_title("Lexical Dispersion Plot")
+        self.ax.set_xlabel("Word Offset")
+        self.ax.grid(False)
