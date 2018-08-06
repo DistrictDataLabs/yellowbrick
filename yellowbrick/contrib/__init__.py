8e739d137fedef94acf0754693c5eeec62e38131
Rebecca Bilbro <bilbro@gmail.com>
2018-08-05 22:41:11 -0400

    make it slightly easier for users to directly import ScatterVisualizer now that it's in contrib

---
+++ yellowbrick/contrib/__init__.py
@@ -0,0 +1,11 @@
+# yellowbrick.contrib
+#
+# Contains a variety of extra tools and experimental visualizers outside of
+# core support or still in development.
+#
+#
+# ID: __init__.py [] bilbro@gmail.com $
+
+
+
+from .scatter import ScatterViz, ScatterVisualizer, scatterviz
