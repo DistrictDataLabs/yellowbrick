79baedcf2ea8c69d1eeed5e207eb994045a6e015
bachwani <nbnb76543@gmail.com>
2019-07-09 15:09:30 +0530

    Few minor changes

--- yellowbrick/features/projection.py
+++ yellowbrick/features/projection.py
@@ -8,7 +8,7 @@ import matplotlib as mpl
 from yellowbrick.draw import manual_legend
 from yellowbrick.features.base import *
 from yellowbrick.exceptions import YellowbrickValueError, NotFitted
-from yellowbrick.exceptions import Yel
+
 
 ##########################################################################
 ## Projection Visualizers
