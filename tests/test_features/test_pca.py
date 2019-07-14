a3128e87d394da5b5204598a1c2e73b5a612a2e2
Piyush Gautam <gautampiyus@gmail.com>
2019-07-15 05:00:26 +0530

    changes the test files and images

--- tests/test_features/test_pca.py
+++ tests/test_features/test_pca.py
@@ -22,7 +22,7 @@ import pytest
 import numpy as np
 
 from unittest import mock
-from tests.dataset import Dataset
+from ..fixtures import Dataset
 from tests.base import VisualTestCase
 
 from yellowbrick.features.pca import *
