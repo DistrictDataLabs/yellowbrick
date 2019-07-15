84496d36d53096bfa3619a53c29488176a5fa972
rebeccabilbro <bilbro@gmail.com>
2019-07-15 11:57:54 -0500

    adds in missing docstrings and includes some gentle linting

--- tests/test_features/test_pca.py
+++ tests/test_features/test_pca.py
@@ -30,6 +30,7 @@ from yellowbrick.exceptions import YellowbrickError
 
 from sklearn.datasets import make_classification
 
+# Note: this can be removed when we deprecate mpl in #826
 try:
     # Only available in Matplotlib >= 2.0.2
     from mpl_toolkits.axes_grid1 import make_axes_locatable
@@ -40,15 +41,22 @@ except ImportError:
 ## Fixtures
 ##########################################################################
 
-@pytest.fixture(scope='class')
+
+@pytest.fixture(scope="class")
 def binary(request):
     """
     Creates a fixture of train and test splits for the sklearn digits dataset
     For ease of use returns a Dataset named tuple composed of two Split tuples.
     """
     X, y = make_classification(
-        n_samples=400, n_features=12, n_informative=8, n_redundant=0,
-        n_classes=2, n_clusters_per_class=1, class_sep=1.8, random_state=854,
+        n_samples=400,
+        n_features=12,
+        n_informative=8,
+        n_redundant=0,
+        n_classes=2,
+        n_clusters_per_class=1,
+        class_sep=1.8,
+        random_state=854,
         scale=[14.2, 2.1, 0.32, 0.001, 32.3, 44.1, 102.3, 2.3, 2.4, 38.2, 0.05, 1.0],
     )
 
@@ -60,6 +68,7 @@ def binary(request):
 ##PCA Tests
 ##########################################################################
 
+
 @pytest.mark.usefixtures("binary")
 class TestPCADecomposition(VisualTestCase):
     """
@@ -67,7 +76,7 @@ class TestPCADecomposition(VisualTestCase):
     """
 
     @pytest.mark.xfail(
-        sys.platform == 'win32', reason="images not close on windows (RMSE=4)"
+        sys.platform == "win32", reason="images not close on windows (RMSE=4)"
     )
     def test_pca_decomposition_quick_method(self):
         """
@@ -79,13 +88,13 @@ class TestPCADecomposition(VisualTestCase):
         self.assert_images_similar(ax=ax, tol=5)
 
     @pytest.mark.xfail(
-        sys.platform == 'win32', reason="images not close on windows (RMSE=?)"
+        sys.platform == "win32", reason="images not close on windows (RMSE=?)"
     )
     def test_scale_true_2d(self):
         """
         Test the PCADecomposition visualizer 2 dimensions scaled.
         """
-        params = {'scale': True, 'proj_dim': 2, 'random_state': 9932}
+        params = {"scale": True, "proj_dim": 2, "random_state": 9932}
         visualizer = PCADecomposition(**params).fit(self.dataset.X)
         pca_array = visualizer.transform(self.dataset.X)
 
@@ -99,7 +108,7 @@ class TestPCADecomposition(VisualTestCase):
         """
         Test the PCADecomposition visualizer 2 dimensions non-scaled.
         """
-        params = {'scale': False, 'proj_dim': 2, 'random_state': 1229}
+        params = {"scale": False, "proj_dim": 2, "random_state": 1229}
         visualizer = PCADecomposition(**params).fit(self.dataset.X)
         pca_array = visualizer.transform(self.dataset.X)
 
@@ -110,15 +119,17 @@ class TestPCADecomposition(VisualTestCase):
         assert pca_array.shape == (self.dataset.X.shape[0], 2)
 
     @pytest.mark.xfail(
-        sys.platform == 'win32', reason="images not close on windows (RMSE=3)"
+        sys.platform == "win32", reason="images not close on windows (RMSE=3)"
     )
     def test_biplot_2d(self):
         """
         Test the PCADecomposition 2D biplot (proj_features).
         """
         params = {
-            'features': 'ABCDEFGHIKLM', 'random_state': 67,
-            'proj_features': True, 'proj_dim': 2,
+            "features": "ABCDEFGHIKLM",
+            "random_state": 67,
+            "proj_features": True,
+            "proj_dim": 2,
         }
         visualizer = PCADecomposition(**params).fit(self.dataset.X)
         pca_array = visualizer.transform(self.dataset.X)
@@ -133,7 +144,7 @@ class TestPCADecomposition(VisualTestCase):
         """
         Test the PCADecomposition visualizer 3 dimensions scaled.
         """
-        params = {'scale': True, 'proj_dim': 3, 'random_state': 7382}
+        params = {"scale": True, "proj_dim": 3, "random_state": 7382}
         visualizer = PCADecomposition(**params).fit(self.dataset.X)
         pca_array = visualizer.transform(self.dataset.X)
 
@@ -147,7 +158,7 @@ class TestPCADecomposition(VisualTestCase):
         """
         Test the PCADecomposition visualizer 3 dimensions non-scaled.
         """
-        params = {'scale': False, 'proj_dim': 3, 'random_state': 98}
+        params = {"scale": False, "proj_dim": 3, "random_state": 98}
         visualizer = PCADecomposition(**params).fit(self.dataset.X)
         pca_array = visualizer.transform(self.dataset.X)
 
@@ -158,15 +169,17 @@ class TestPCADecomposition(VisualTestCase):
         assert pca_array.shape == (self.dataset.X.shape[0], 3)
 
     @pytest.mark.xfail(
-        sys.platform == 'win32', reason="images not close on windows (RMSE=3)"
+        sys.platform == "win32", reason="images not close on windows (RMSE=3)"
     )
     def test_biplot_3d(self):
         """
         Test the PCADecomposition 3D biplot (proj_features).
         """
         params = {
-            'features': 'ABCDEFGHIKLM', 'random_state': 800,
-            'proj_features': True, 'proj_dim': 3,
+            "features": "ABCDEFGHIKLM",
+            "random_state": 800,
+            "proj_features": True,
+            "proj_dim": 3,
         }
         visualizer = PCADecomposition(**params).fit(self.dataset.X)
         pca_array = visualizer.transform(self.dataset.X)
@@ -179,31 +192,31 @@ class TestPCADecomposition(VisualTestCase):
 
     def test_scale_true_4d_execption(self):
         """
-        Test the PCADecomposition visualizer 4 dimensions scaled (catch YellowbrickError).
+        Test PCADecomposition visualizer 4 dimensions scaled (catch YellowbrickError).
         """
-        params = {'scale': True, 'proj_dim': 4}
+        params = {"scale": True, "proj_dim": 4}
         with pytest.raises(YellowbrickError, match="proj_dim object is not 2 or 3"):
             PCADecomposition(**params)
 
     def test_scale_true_3d_execption(self):
         """
-        Test the PCADecomposition visualizer 3 dims scaled on 2 dim data set (catch ValueError).
+        Test PCADecomposition visualizer 3 dims scaled on 2 dim data set (catch ValueError).
         """
         X = np.random.normal(loc=2, size=(100, 2))
-        params = {'scale': True, 'proj_dim': 3}
+        params = {"scale": True, "proj_dim": 3}
 
-        e = r'n_components=3 must be between 0 and min\(n_samples, n_features\)=2'
+        e = r"n_components=3 must be between 0 and min\(n_samples, n_features\)=2"
         with pytest.raises(ValueError, match=e):
             pca = PCADecomposition(**params)
             pca.fit(X)
 
-    @mock.patch('yellowbrick.features.pca.plt.sca', autospec=True)
+    @mock.patch("yellowbrick.features.pca.plt.sca", autospec=True)
     def test_alpha_param(self, mock_sca):
         """
         Test that the user can supply an alpha param on instantiation
         """
         # Instantiate a prediction error plot, provide custom alpha
-        params = {'alpha': 0.3, 'proj_dim': 2, 'random_state': 9932}
+        params = {"alpha": 0.3, "proj_dim": 2, "random_state": 9932}
         visualizer = PCADecomposition(**params).fit(self.dataset.X)
         pca_array = visualizer.transform(self.dataset.X)
         assert visualizer.alpha == 0.3
@@ -217,25 +230,35 @@ class TestPCADecomposition(VisualTestCase):
         assert "alpha" in scatter_kwargs
         assert scatter_kwargs["alpha"] == 0.3
         assert pca_array.shape == (self.dataset.X.shape[0], 2)
-        
+
     def test_colorbar(self):
         """
         Test the PCADecomposition visualizer's colorbar features.
         """
-        params = {'scale': True, 'proj_dim': 2, 'random_state': 7382, 
-                   'color': self.dataset.y, 'colorbar': True}
+        params = {
+            "scale": True,
+            "proj_dim": 2,
+            "random_state": 7382,
+            "color": self.dataset.y,
+            "colorbar": True,
+        }
         visualizer = PCADecomposition(**params).fit(self.dataset.X)
         visualizer.transform(self.dataset.X)
         visualizer.uax.set_xticklabels([])
         # Image comparison tests
-        self.assert_images_similar(visualizer)  
+        self.assert_images_similar(visualizer)
 
     def test_heatmap(self):
         """
         Test the PCADecomposition visualizer's heatmap features.
         """
-        params = {'scale': True, 'proj_dim': 2, 'random_state': 7382, 
-                   'color': self.dataset.y, 'heatmap': True}
+        params = {
+            "scale": True,
+            "proj_dim": 2,
+            "random_state": 7382,
+            "color": self.dataset.y,
+            "heatmap": True,
+        }
         visualizer = PCADecomposition(**params).fit(self.dataset.X)
         visualizer.transform(self.dataset.X)
         visualizer.finalize()
@@ -248,10 +271,16 @@ class TestPCADecomposition(VisualTestCase):
 
     def test_colorbar_heatmap(self):
         """
-        Test the PCADecomposition visualizer's colorbar features.
+        Test the PCADecomposition visualizer with both colorbar and heatmap.
         """
-        params = {'scale': True, 'proj_dim': 2, 'random_state': 7382, 
-                   'color': self.dataset.y, 'colorbar': True, 'heatmap': True}
+        params = {
+            "scale": True,
+            "proj_dim": 2,
+            "random_state": 7382,
+            "color": self.dataset.y,
+            "colorbar": True,
+            "heatmap": True,
+        }
         visualizer = PCADecomposition(**params).fit(self.dataset.X)
         visualizer.transform(self.dataset.X)
         visualizer.finalize()
@@ -272,9 +301,10 @@ class TestPCADecomposition(VisualTestCase):
 
         with pytest.raises(YellowbrickValueError):
             PCADecomposition(proj_dim=3, heatmap=True)
-        
 
-    @pytest.mark.skipif(make_axes_locatable is not None, reason="requires matplotlib <= 2.0.1")
+    @pytest.mark.skipif(
+        make_axes_locatable is not None, reason="requires matplotlib <= 2.0.1"
+    )
     def test_matplotlib_version_error():
         """
         Assert an exception is raised with incompatible matplotlib versions
--- yellowbrick/features/pca.py
+++ yellowbrick/features/pca.py
@@ -20,7 +20,7 @@ Decomposition based feature visualization with PCA.
 # NOTE: must import mplot3d to load the 3D projection
 import numpy as np
 import matplotlib.pyplot as plt
-import mpl_toolkits.mplot3d # noqa
+import mpl_toolkits.mplot3d  # noqa
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from yellowbrick.features.base import MultiFeatureVisualizer
@@ -33,8 +33,9 @@ from sklearn.preprocessing import StandardScaler
 
 
 ##########################################################################
-##2D and #3D PCA Visualizer
+# 2D and 3D PCA Visualizer
 ##########################################################################
+
 
 class PCADecomposition(MultiFeatureVisualizer):
     """
@@ -71,7 +72,7 @@ class PCADecomposition(MultiFeatureVisualizer):
         Optional string or matplotlib cmap to colorize lines.
         Use either color to colorize the lines on a per class basis or
         colormap to color them on a continuous scale.
-    
+
     alpha : float, default: 0.75
         Specify a transparency where 1 is completely opaque and 0 is completely
         transparent. This property makes densely clustered points more visible.
@@ -83,10 +84,11 @@ class PCADecomposition(MultiFeatureVisualizer):
         the random state on this solver.
 
     colorbar : bool, default: False
-        Boolean to enable the colorbar in the scatter plot.
+        Add a colorbar to shows the range in magnitude of feature values to the
+        component.
 
     heatmap : bool, default: False
-        Boolean to enable the heatmap in the scatter plot.
+        Add a heatmap to explain which features contribute most to which component.
 
     kwargs : dict
         Keyword arguments that are passed to the base class and may influence
@@ -103,22 +105,23 @@ class PCADecomposition(MultiFeatureVisualizer):
     >>> visualizer.poof()
 
     """
-    def __init__(self,
-                 ax=None,
-                 features=None,
-                 scale=True,
-                 proj_dim=2,
-                 proj_features=False,
-                 color=None,
-                 colormap=palettes.DEFAULT_SEQUENCE,
-                 alpha=0.75,
-                 random_state=None,
-                 colorbar=False,
-                 heatmap=False,
-                 **kwargs):
-        super(PCADecomposition, self).__init__(ax=ax,
-                                               features=features,
-                                               **kwargs)
+
+    def __init__(
+        self,
+        ax=None,
+        features=None,
+        scale=True,
+        proj_dim=2,
+        proj_features=False,
+        color=None,
+        colormap=palettes.DEFAULT_SEQUENCE,
+        alpha=0.75,
+        random_state=None,
+        colorbar=False,
+        heatmap=False,
+        **kwargs
+    ):
+        super(PCADecomposition, self).__init__(ax=ax, features=features, **kwargs)
 
         if proj_dim not in (2, 3):
             raise YellowbrickValueError("proj_dim object is not 2 or 3.")
@@ -130,22 +133,26 @@ class PCADecomposition(MultiFeatureVisualizer):
 
         # Create the PCA transformer
         self.pca_transformer = Pipeline(
-            [('scale', StandardScaler(with_std=self.scale)),
-             ('pca', PCA(self.proj_dim, random_state=random_state))]
+            [
+                ("scale", StandardScaler(with_std=self.scale)),
+                ("pca", PCA(self.proj_dim, random_state=random_state)),
+            ]
         )
         self.alpha = alpha
-        
+
         # Visual Parameters
         self.color = color
         self.colormap = colormap
-        self.colorbar = colorbar 
+        self.colorbar = colorbar
         self.heatmap = heatmap
-        
+
         self.uax, self.lax = None, None
-        
+
         if self.proj_dim == 3 and (self.heatmap or self.colorbar):
-            raise YellowbrickValueError("heatmap and colorbar are not compatible with 3d projections")
-        
+            raise YellowbrickValueError(
+                "heatmap and colorbar are not compatible with 3d projections"
+            )
+
         if self.heatmap or self.colorbar:
             self._layout()
 
@@ -153,7 +160,7 @@ class PCADecomposition(MultiFeatureVisualizer):
         """
         Creates the layout for colorbar and heatmap, adding new axes for the heatmap
         if necessary and modifying the aspect ratio. Does not modify the axes or the
-        layout if self.heatmap is False or None.
+        layout if ``self.heatmap`` is ``False`` or ``None``.
         """
         # Ensure the axes are created if not heatmap, then return.
 
@@ -163,10 +170,12 @@ class PCADecomposition(MultiFeatureVisualizer):
 
         # Ensure matplotlib version compatibility
         if make_axes_locatable is None:
-            raise YellowbrickValueError((
-                "heatmap requires matplotlib 2.0.2 or greater "
-                "please upgrade matplotlib or set heatmap=False on the visualizer"
-            ))
+            raise YellowbrickValueError(
+                (
+                    "heatmap requires matplotlib 2.0.2 or greater "
+                    "please upgrade matplotlib or set heatmap=False on the visualizer"
+                )
+            )
 
         # Create the new axes for the colorbar and heatmap
         divider = make_axes_locatable(self.ax)
@@ -195,26 +204,72 @@ class PCADecomposition(MultiFeatureVisualizer):
         """
         super(PCADecomposition, self).fit(X=X, y=y, **kwargs)
         self.pca_transformer.fit(X)
-        self.pca_components_ = self.pca_transformer.named_steps['pca'].components_
+        self.pca_components_ = self.pca_transformer.named_steps["pca"].components_
         return self
 
     def transform(self, X, y=None, **kwargs):
-        
+        """
+        Calls the internal `transform` method of the scikit-learn PCA transformer, which
+        performs a dimensionality reduction on the input features ``X``. Next calls the
+        ``draw`` method of the Yellowbrick visualizer, finally returning a new array of
+        transformed features of shape ``(len(X), proj_dim)``.
+
+        Parameters
+        ----------
+        X : ndarray or DataFrame of shape n x m
+            A matrix of n instances with m features.
+
+        y : ndarray or Series of length n
+            An array or series of target or class values.
+
+        Returns
+        -------
+        pca_features_ : ndarray or DataFrame of shape n x m
+            Returns a new array-like object of transformed features of shape
+            ``(len(X), proj_dim)``.
+        """
         self.pca_features_ = self.pca_transformer.transform(X)
         self.draw()
         return self.pca_features_
 
     def draw(self, **kwargs):
+        """
+        Plots a scatterplot of points that represented the decomposition,
+        `pca_features_`, of the original features, `X`, projected into either 2 or
+        3 dimensions.
 
+        If 2 dimensions are selected, a colorbar and heatmap can also be optionally
+        included to show the magnitude of each feature value to the component.
+
+        Returns
+        -------
+        self : visualizer.ax
+            Returns the axes of the visualizer for use in Pipelines
+        """
         X = self.pca_features_
         if self.proj_dim == 2:
-            im = self.ax.scatter(X[:, 0], X[:, 1], c=self.color, cmap=self.colormap, alpha=self.alpha,
-                    vmin= self.pca_components_.min(), vmax = self.pca_components_.max(), **kwargs)
+            im = self.ax.scatter(
+                X[:, 0],
+                X[:, 1],
+                c=self.color,
+                cmap=self.colormap,
+                alpha=self.alpha,
+                vmin=self.pca_components_.min(),
+                vmax=self.pca_components_.max(),
+                **kwargs
+            )
             if self.colorbar:
-                plt.colorbar(im, cax=self.uax, orientation='horizontal',
-                    ticks=[self.pca_components_.min(), 0, self.pca_components_.max()])
+                plt.colorbar(
+                    im,
+                    cax=self.uax,
+                    orientation="horizontal",
+                    ticks=[self.pca_components_.min(), 0, self.pca_components_.max()],
+                )
             if self.heatmap:
-                self.lax.imshow(self.pca_components_, interpolation='none', cmap=self.colormap)
+                # TODO: change to pcolormesh instead of imshow per #615 spec
+                self.lax.imshow(
+                    self.pca_components_, interpolation="none", cmap=self.colormap
+                )
             if self.proj_features:
                 x_vector = self.pca_components_[0]
                 y_vector = self.pca_components_[1]
@@ -222,22 +277,31 @@ class PCADecomposition(MultiFeatureVisualizer):
                 max_y = max(X[:, 1])
                 for i in range(self.pca_components_.shape[1]):
                     self.ax.arrow(
-                        x=0, y=0,
+                        x=0,
+                        y=0,
                         dx=x_vector[i] * max_x,
                         dy=y_vector[i] * max_y,
-                        color='r', head_width=0.05,
+                        color="r",
+                        head_width=0.05,
                         width=0.005,
                     )
                     self.ax.text(
                         x_vector[i] * max_x * 1.05,
                         y_vector[i] * max_y * 1.05,
-                        self.features_[i], color='r'
+                        self.features_[i],
+                        color="r",
                     )
         if self.proj_dim == 3:
             self.fig = plt.figure()
-            self.ax = self.fig.add_subplot(111, projection='3d')
-            self.ax.scatter(X[:, 0], X[:, 1], X[:, 2],
-                            c=self.color, cmap=self.colormap, alpha=self.alpha)
+            self.ax = self.fig.add_subplot(111, projection="3d")
+            self.ax.scatter(
+                X[:, 0],
+                X[:, 1],
+                X[:, 2],
+                c=self.color,
+                cmap=self.colormap,
+                alpha=self.alpha,
+            )
             if self.proj_features:
                 x_vector = self.pca_components_[0]
                 y_vector = self.pca_components_[1]
@@ -250,42 +314,64 @@ class PCADecomposition(MultiFeatureVisualizer):
                         [0, x_vector[i] * max_x],
                         [0, y_vector[i] * max_y],
                         [0, z_vector[i] * max_z],
-                        color='r'
+                        color="r",
                     )
                     self.ax.text(
                         x_vector[i] * max_x * 1.05,
                         y_vector[i] * max_y * 1.05,
                         z_vector[i] * max_z * 1.05,
-                        self.features_[i], color='r'
+                        self.features_[i],
+                        color="r",
                     )
         return self.ax
 
     def finalize(self, **kwargs):
-        # Set the title
-        self.ax.set_title('Principal Component Plot')
-        self.ax.set_xlabel('Principal Component 1', linespacing=1)
-        self.ax.set_ylabel('Principal Component 2', linespacing=1.2)
+        """
+        Draws the title, labels, legends, heatmap, and colorbar as specified by the
+        keyword arguments.
+        """
+        self.ax.set_title("Principal Component Plot")
+        self.ax.set_xlabel("Principal Component 1", linespacing=1)
+        self.ax.set_ylabel("Principal Component 2", linespacing=1.2)
         if self.heatmap == True:
-            self.lax.set_xticks(np.arange(-.5, len(self.features_)))
-            self.lax.set_xticklabels(self.features_, rotation=90, ha='left', fontsize=12)
+            self.lax.set_xticks(np.arange(-0.5, len(self.features_)))
+            self.lax.set_xticklabels(
+                self.features_, rotation=90, ha="left", fontsize=12
+            )
             self.lax.set_yticks(np.arange(0.5, 2))
-            self.lax.set_yticklabels(['First PC', 'Second PC'], va='bottom', fontsize=12)
+            self.lax.set_yticklabels(
+                ["First PC", "Second PC"], va="bottom", fontsize=12
+            )
         if self.proj_dim == 3:
-            self.ax.set_zlabel('Principal Component 3', linespacing=1.2)
+            self.ax.set_zlabel("Principal Component 3", linespacing=1.2)
+
 
 ##########################################################################
 ## Quick Method
 ##########################################################################
 
-def pca_decomposition(X, y=None, ax=None, features=None, scale=True,
-                      proj_dim=2, proj_features=False, color=None,
-                      colormap=palettes.DEFAULT_SEQUENCE, alpha=0.75,
-                      random_state=None, colorbar=False, heatmap=False,
-                       **kwargs):
-    """Produce a two or three dimensional principal component plot of the data array ``X``
-    projected onto it's largest sequential principal components. It is common practice to scale the
-    data array ``X`` before applying a PC decomposition. Variable scaling can be controlled using
-    the ``scale`` argument.
+
+def pca_decomposition(
+    X,
+    y=None,
+    ax=None,
+    features=None,
+    scale=True,
+    proj_dim=2,
+    proj_features=False,
+    color=None,
+    colormap=palettes.DEFAULT_SEQUENCE,
+    alpha=0.75,
+    random_state=None,
+    colorbar=False,
+    heatmap=False,
+    **kwargs
+):
+    """
+    Produce a two or three dimensional principal component plot of the data array ``X``
+    projected onto it's largest sequential principal components. It is common practice
+    to scale the data array ``X`` before applying a PC decomposition. Variable scaling
+    can be controlled using the ``scale`` argument.
 
     Parameters
     ----------
@@ -321,7 +407,7 @@ def pca_decomposition(X, y=None, ax=None, features=None, scale=True,
         Optional string or matplotlib cmap to colorize lines.
         Use either color to colorize the lines on a per class basis or
         colormap to color them on a continuous scale.
-        
+
     alpha : float, default: 0.75
         Specify a transparency where 1 is completely opaque and 0 is completely
         transparent. This property makes densely clustered points more visible.
@@ -331,12 +417,13 @@ def pca_decomposition(X, y=None, ax=None, features=None, scale=True,
         extract is lower than 80% of the smallest dimension of the data, then
         the more efficient `randomized` solver is enabled, this parameter sets
         the random state on this solver.
-    
+
     colorbar : bool, default: False
-        Boolean to enable or disable the colorbar feature in the scatter plot.
+        Add a colorbar to shows the range in magnitude of feature values to the
+        component.
 
     heatmap : bool, default: False
-        Boolean to enable or disable the heatmap feature in the scatter plot.
+        Add a heatmap to explain which features contribute most to which component.
 
     kwargs : dict
         Keyword arguments that are passed to the base class and may influence
@@ -353,10 +440,18 @@ def pca_decomposition(X, y=None, ax=None, features=None, scale=True,
     """
     # Instantiate the visualizer
     visualizer = PCADecomposition(
-        ax=ax, features=features, scale=scale, proj_dim=proj_dim,
-        proj_features=proj_features, color=color, colormap=colormap,
-        alpha=alpha, random_state=random_state, colorbar=colorbar, 
-        heatmap=heatmap, **kwargs
+        ax=ax,
+        features=features,
+        scale=scale,
+        proj_dim=proj_dim,
+        proj_features=proj_features,
+        color=color,
+        colormap=colormap,
+        alpha=alpha,
+        random_state=random_state,
+        colorbar=colorbar,
+        heatmap=heatmap,
+        **kwargs
     )
 
     # Fit and transform the visualizer (calls draw)
