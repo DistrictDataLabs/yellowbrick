80588c208de85ecefe55a50b9c45290d662a18b2
Benjamin Bengfort <benjamin@bengfort.com>
2019-07-27 11:44:01 -0400

    patch exception tests

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,18 +1,26 @@
 ##########################################################################
 ## Imports
 ##########################################################################
+
 import pytest
+
 from yellowbrick.exceptions import NotFitted
+
 
 ##########################################################################
 ## NotFitted Exception Tests
 ##########################################################################
-class TestNotFitted(object):
+
+class TestExceptions(object):
     """
-    Test Not Fitted Error
-    """    
-    def test_correct_message(self):
-        "Ensure that NotFitted error is raised properly"
+    Test exception specific code and utilities
+    """
+
+    @pytest.mark.parametrize("method", ['transform', None])
+    def test_not_fitted_from_estimator(self, method):
+        """
+        Ensure not fitted can be raised directly from an estimator
+        """
         msg = "instance is not fitted yet, please call fit"
         with pytest.raises(NotFitted, match=msg):
-            raise NotFitted.from_estimator(self, 'transform')
+            raise NotFitted.from_estimator(self, method)
