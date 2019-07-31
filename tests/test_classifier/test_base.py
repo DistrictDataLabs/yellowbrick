1764b44a831596e161678448bf3760dd74be6f49
Benjamin Bengfort <benjamin@bengfort.com>
2019-07-31 11:25:50 -0400

    this will break tests

---
+++ tests/test_classifier/test_base.py
@@ -0,0 +1,44 @@
+# tests.test_classifier.test_base
+# Tests for the base classification visualizers
+#
+# Author:   Benjamin Bengfort
+# Created:  Wed Jul 31 11:21:28 2019 -0400
+#
+# Copyright (C) 2019 The scikit-yb developers
+# For license information, see LICENSE.txt
+#
+# ID: test_base.py [] benjamin@bengfort.com $
+
+"""
+Tests for the base classification visualizers
+"""
+
+##########################################################################
+## Imports
+##########################################################################
+
+import pytest
+
+from yellowbrick.classifier.base import *
+
+
+##########################################################################
+## Test Classification Score Visualizer
+##########################################################################
+
+@pytest.mark.usefixtures('binary', 'multiclass')
+class TestClassificationScoreVisualizer(object):
+
+    def test_fit(self):
+        """
+        Ensure that classes and class counts are computed on fit
+        """
+
+        oz = ClassificationScoreVisualizer()
+        assert not hasattr(oz, 'classes_')
+        assert not hasattr(oz, 'class_count_')
+        assert not hasattr(oz, 'score_')
+        assert oz.fit(self.binary.X, self.binary.y) is oz
+        assert hasattr(oz, 'classes_')
+        assert hasattr(oz, 'class_count_')
+        assert not hasattr(oz, 'score_')
--- yellowbrick/classifier/base.py
+++ yellowbrick/classifier/base.py
@@ -23,8 +23,7 @@ import numpy as np
 
 from ..utils import isclassifier
 from ..base import ScoreVisualizer
-from ..style.palettes import color_palette
-from ..exceptions import YellowbrickTypeError
+from ..exceptions import YellowbrickTypeError, YellowbrickValueError
 
 
 ##########################################################################
@@ -33,16 +32,81 @@ from ..exceptions import YellowbrickTypeError
 
 
 class ClassificationScoreVisualizer(ScoreVisualizer):
-    def __init__(self, model, ax=None, fig=None, classes=None, **kwargs):
-        """
-        Check to see if model is an instance of a classifer.
-        Should return an error if it isn't.
+    """Base class for classifier model selection.
 
-        .. todo:: document this class.
-        .. todo:: accept as input ``classes``, as all visualizers need this.
-        """
+    The ClassificationScoreVisualizer wraps a classifier to produce a
+    visualization when the score method is called, usually to allow the user
+    to effectively compare the performance between models.
+
+    The base class provides helper functionality to ensure that classification
+    visualizers are able to correctly identify and encode classes with human
+    readable labels and to map colors to the classes if required.
+
+    Parameters
+    ----------
+    model : estimator
+        A scikit-learn estimator that should be a classifier. If the model is
+        not a classifier, an exception is raised.
+
+    ax : matplotlib Axes, default: None
+        The axis to plot the figure on. If None is passed in the current axes
+        will be used (or generated if required).
+
+    fig : matplotlib Figure, default: None
+        The figure to plot the Visualizer on. If None is passed in the current
+        plot will be used (or generated if required).
+
+    classes : list of str, defult: None
+        The class labels to use for the legend ordered by the index of the
+        classes discovered in the ``fit()`` method. Specifying classes in this
+        manner is used to change the class names to a more specific format or
+        to label encoded integer classes. Some visualizers may also use this
+        field to filter the visualization for specific classes. For more advanced
+        usage specify an encoder rather than class labels.
+
+    encoder : dict or LabelEncoder, default: None
+        A mapping of classes to human readable labels. Often there is a mismatch
+        between desired class labels and those contained in the target variable
+        passed to ``fit()`` or ``score()``. The encoder disambiguates this mismatch
+        ensuring that classes are labeled correctly in the visualization.
+
+    force_model : bool, default: False
+        Do not check to ensure that the underlying estimator is a classifier. This
+        will prevent an exception when the visualizer is initialized but may result
+        in unexpected or unintended behavior.
+
+    Attributes
+    ----------
+    classes_ : ndarray of shape (n_classes,)
+        The class labels observed while fitting.
+
+    class_count_ : ndarray of shape (n_classes,)
+        Number of samples encountered for each class during fitting.
+
+    score_ : float
+        An evaluation metric of the classifier on test data produced when
+        ``score()`` is called. This metric is between 0 and 1 -- higher scores are
+        generally better. For classifiers, this score is usually accuracy, but
+        ensure you check the underlying model for more details about the score.
+
+    Notes
+    -----
+    Classification score visualizers should implement the ``score()``
+    and ``draw()`` visualier methods.
+    """
+
+    def __init__(
+        self,
+        model,
+        ax=None,
+        fig=None,
+        classes=None,
+        encoder=None,
+        force_model=False,
+        **kwargs
+    ):
         # A bit of type checking
-        if not isclassifier(model):
+        if not force_model and not isclassifier(model):
             raise YellowbrickTypeError(
                 "This estimator is not a classifier; "
                 "try a regression or clustering score visualizer instead!"
@@ -53,49 +117,23 @@ class ClassificationScoreVisualizer(ScoreVisualizer):
             model, ax=ax, fig=fig, **kwargs
         )
 
-        # Convert to array if necessary to match estimator.classes_
-        if classes is not None:
-            classes = np.array(classes)
-
-        # Set up classifier score visualization properties
-        if classes is not None:
-            n_colors = len(classes)
-        else:
-            n_colors = None
-
-        self.colors = color_palette(kwargs.pop("colors", None), n_colors)
-        self.classes_ = classes
-
-    @property
-    def classes_(self):
-        """
-        Proxy property to smartly access the classes from the estimator or
-        stored locally on the score visualizer for visualization.
-        """
-        if self.__classes is None:
-            try:
-                return self.estimator.classes_
-            except AttributeError:
-                return None  # TODO: raise NotFittedError instead of returning None
-        return self.__classes
-
-    @classes_.setter
-    def classes_(self, value):
-        self.__classes = value
+        self.set_params(
+            classes=classes,
+            encoder=encoder,
+            force_model=force_model,
+        )
 
     def fit(self, X, y=None, **kwargs):
         """
+        Fit the visualizer to the specified data.
+
         Parameters
         ----------
-
         X : ndarray or DataFrame of shape n x m
             A matrix of n instances with m features
 
         y : ndarray or Series of length n
             An array or series of target or class values
-
-        kwargs: dict
-            Keyword arguments passed to Scikit-Learn API.
 
         Returns
         -------
@@ -103,12 +141,19 @@ class ClassificationScoreVisualizer(ScoreVisualizer):
             Returns the instance of the classification score visualizer
 
         """
-        # Fit the inner estimator
-        self.estimator.fit(X, y)
+        # Super fits the wrapped estimator
+        super(ClassificationScoreVisualizer, self).fit(X, y)
 
-        # Extract the classes from the estimator
-        if self.classes_ is None:
-            self.classes_ = self.estimator.classes_
+        # Extract the classes and the class counts from the target
+        self.classes_, self.class_counts_ = np.unique(y, return_counts=True)
+
+        # Ensure the classes are aligned with the estimator
+        # TODO: should we simply warn here, why would this be the case?
+        if hasattr(self.estimator, 'classes_'):
+            if not np.all(self.classes_ == self.estimator.classes_, axis=0):
+                raise YellowbrickValueError(
+                    "unique classes in y do not match estimator.classes_"
+                )
 
         # Always return self from fit
         return self
@@ -125,18 +170,16 @@ class ClassificationScoreVisualizer(ScoreVisualizer):
         ----------
         X : array-like
             X (also X_test) are the dependent variables of test set to predict
+
         y : array-like
             y (also y_test) is the independent actual variables to score against
 
         Returns
         -------
         score : float
+            Returns the score of the underlying model, usually accuracy for
+            classification models. Refer to the specific model for more details.
         """
+        # This method implements ScoreVisualizer (do not call super).
         self.score_ = self.estimator.score(X, y, **kwargs)
-
         return self.score_
-
-    # TODO during refactoring this can be used to generalize ClassBalance
-    def class_counts(self, y):
-        unique, counts = np.unique(y, return_counts=True)
-        return dict(zip(unique, counts))
