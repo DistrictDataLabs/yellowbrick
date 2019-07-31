98b7e97002d79b18e485de03610dc0afe8fe11fa
Rebecca Bilbro <rebeccabilbro@users.noreply.github.com>
2019-07-30 21:05:38 -0400

    Revert to manually generating UMAP visualizations in docs (#933)
    Reverts to manually generating the docs for the UMAP Visualizer and also updates the class docstrings for readability

--- yellowbrick/text/umap_vis.py
+++ yellowbrick/text/umap_vis.py
@@ -4,10 +4,9 @@
 # Author:   John Healy <jchealy@gmail.com>
 # Created:  Mon Dec 03 14:00:00 2018 -0500
 #
-# Copyright (C) 2016 Bengfort.com
+# Copyright (C) 2019 The sckit-yb developers
 # For license information, see LICENSE.txt
 #
-
 
 """
 Implements UMAP visualizations of documents in 2D space.
@@ -43,22 +42,24 @@ except (RuntimeError, AttributeError):
 ## Quick Methods
 ##########################################################################
 
-def umap(X, y=None, ax=None, classes=None,
-           colors=None, colormap=None, alpha=0.7, **kwargs):
+
+def umap(
+    X, y=None, ax=None, classes=None, colors=None, colormap=None, alpha=0.7, **kwargs
+):
     """
     Display a projection of a vectorized corpus in two dimensions using UMAP (Uniform
-    Manifold Approximation and Projection),
-    a nonlinear dimensionality reduction method that is particularly well
-    suited to embedding in two or three dimensions for visualization as a
-    scatter plot. UMAP is a relatively new technique but is often used to visualize clusters or
-    groups of data points and their relative proximities.  It typically is fast, scalable,
-    and can be applied directly to sparse matrices eliminating the need to run a truncatedSVD
-    as a pre-processing step.
+    Manifold Approximation and Projection), a nonlinear dimensionality reduction method
+    that is particularly well suited to embedding in two or three dimensions for
+    visualization as a scatter plot. UMAP is a relatively new technique but is often
+    used to visualize clusters or groups of data points and their relative proximities.
+    It typically is fast, scalable, and can be applied directly to sparse matrices
+    eliminating the need to run a ``TruncatedSVD`` as a pre-processing step.
 
-    The current default for UMAP is Euclidean distance.  Hellinger distance would be a more appropriate distance
-    function to use with CountVectorize data.  That will be released in the next version of UMAP.
-    In the meantime cosine distance is likely a better text default that Euclidean and can be set using
-    the keyword arg metric='cosine'.
+    The current default for UMAP is Euclidean distance. Hellinger distance would be a
+    more appropriate distance function to use with CountVectorize data. That will be
+    released in a forthcoming version of UMAP. In the meantime cosine distance is likely
+    a better text default that Euclidean and can be set using the keyword argument
+    ``metric='cosine'``.
 
     Parameters
     ----------
@@ -99,13 +100,10 @@ def umap(X, y=None, ax=None, classes=None,
         Returns the axes that the parallel coordinates were drawn on.
     """
     # Instantiate the visualizer
-    visualizer = UMAPVisualizer(
-        ax, classes, colors, colormap, alpha, **kwargs
-    )
+    visualizer = UMAPVisualizer(ax, classes, colors, colormap, alpha, **kwargs)
 
     # Fit and transform the visualizer (calls draw)
     visualizer.fit_transform(X, y, **kwargs)
-    #visualizer.transform(X)
 
     # Return the axes object on the visualizer
     return visualizer.ax
@@ -115,33 +113,22 @@ def umap(X, y=None, ax=None, classes=None,
 ## UMAPVisualizer
 ##########################################################################
 
+
 class UMAPVisualizer(TextVisualizer):
     """
-    Display a projection of a vectorized corpus in two dimensions using UMAP,
-    a nonlinear dimensionality reduction method that is particularly well
-    suited to embedding in two or three dimensions for visualization as a
-    scatter plot. UMAP is a relatively new technique but is often used to visualize clusters or
-    groups of data points and their relative proximities.  It typically is fast, scalable,
-    and can be applied directly to sparse matrices eliminating the need to run a truncatedSVD
-    as a pre-processing step.
+    Display a projection of a vectorized corpus in two dimensions using UMAP (Uniform
+    Manifold Approximation and Projection), a nonlinear dimensionality reduction method
+    that is particularly well suited to embedding in two or three dimensions for
+    visualization as a scatter plot. UMAP is a relatively new technique but is often
+    used to visualize clusters or groups of data points and their relative proximities.
+    It typically is fast, scalable, and can be applied directly to sparse matrices
+    eliminating the need to run a ``TruncatedSVD`` as a pre-processing step.
 
-    UMAP will return a scatter plot of the vectorized corpus, such that each
-    point represents a document or utterance. By default, the distance between two points
-    in the visual space is embedded using the cosine distance between the high dimensional feature
-    vectors.  Thus, UMAP shows the clusters of similar documents and the relationships between
-    groups of documents as a scatter plot.
-
-    UMAP can be used with either clustering or classification; by specifying
-    the ``classes`` argument, points will be colored based on their similar
-    traits. For example, by passing ``cluster.labels_`` as ``y`` in ``fit()``, all
-    points in the same cluster will be grouped together. This extends the
-    neighbor embedding with more information about similarity, and can allow
-    better interpretation of both clusters and classes.
-
-    The current default for UMAP is Euclidean distance.  Hellinger distance would be a more appropriate distance
-    function to use with CountVectorize data.  That will be released in the next version of UMAP.
-    In the meantime cosine distance is likely a better text default that Euclidean and can be set using
-    the keyword argument metric='cosine'.
+    The current default for UMAP is Euclidean distance. Hellinger distance would be a
+    more appropriate distance function to use with CountVectorize data. That will be
+    released in a forthcoming version of UMAP. In the meantime cosine distance is likely
+    a better text default that Euclidean and can be set using the keyword argument
+    ``metric='cosine'``.
 
     For more, see https://github.com/lmcinnes/umap
 
@@ -175,7 +162,6 @@ class UMAPVisualizer(TextVisualizer):
     kwargs : dict
         Pass any additional keyword arguments to the UMAP transformer.
 
-
     Examples
     --------
 
@@ -188,16 +174,25 @@ class UMAPVisualizer(TextVisualizer):
     # NOTE: cannot be np.nan
     NULL_CLASS = None
 
-    def __init__(self, ax=None,
-                 labels=None, classes=None, colors=None, colormap=None,
-                 random_state=None, alpha=0.7, **kwargs):
+    def __init__(
+        self,
+        ax=None,
+        labels=None,
+        classes=None,
+        colors=None,
+        colormap=None,
+        random_state=None,
+        alpha=0.7,
+        **kwargs
+    ):
 
         if UMAP is None:
-            raise YellowbrickValueError((
-                "umap package doesn't seem to be installed."
-                "Please install UMAP via: pip install umap-learn"
-            ))
-
+            raise YellowbrickValueError(
+                (
+                    "umap package doesn't seem to be installed."
+                    "Please install UMAP via: pip install umap-learn"
+                )
+            )
 
         # Visual Parameters
         self.alpha = alpha
@@ -206,16 +201,13 @@ class UMAPVisualizer(TextVisualizer):
         self.colormap = colormap
         self.random_state = random_state
 
-
         # Fetch UMAP kwargs from kwargs by popping only keys belonging to UMAP params
         umap_kwargs = {
-            key: kwargs.pop(key)
-            for key in UMAP().get_params()
-            if key in kwargs
+            key: kwargs.pop(key) for key in UMAP().get_params() if key in kwargs
         }
 
-        #UMAP doesn't require any pre-processing before embedding and thus doesn't require a pipeline.
-        #self.transformer_ = UMAP(n_components=2, random_state=random_state, **umap_kwargs)
+        # UMAP doesn't require any pre-processing before embedding and thus doesn't
+        # require a pipeline.
         self.transformer_ = self.make_transformer(umap_kwargs)
 
         # Call super at the end so that size and title are set correctly
@@ -229,6 +221,8 @@ class UMAPVisualizer(TextVisualizer):
 
         Parameters
         ----------
+        umap_kwargs : dict
+            Keyword arguments for the internal UMAP transformer
 
         Returns
         -------
@@ -240,12 +234,15 @@ class UMAPVisualizer(TextVisualizer):
         steps = []
 
         # Add the UMAP manifold
-        steps.append(('umap', UMAP(
-            n_components=2, random_state=self.random_state, **umap_kwargs)))
+        steps.append(
+            (
+                "umap",
+                UMAP(n_components=2, random_state=self.random_state, **umap_kwargs),
+            )
+        )
 
         # return the pipeline
         return Pipeline(steps)
-
 
     def fit(self, X, y=None, **kwargs):
         """
@@ -305,15 +302,17 @@ class UMAPVisualizer(TextVisualizer):
         # Resolve the labels with the classes
         labels = self.labels if self.labels is not None else self.classes_
         if len(labels) != len(self.classes_):
-            raise YellowbrickValueError((
-                "number of supplied labels ({}) does not "
-                "match the number of classes ({})"
-            ).format(len(labels), len(self.classes_)))
-
+            raise YellowbrickValueError(
+                (
+                    "number of supplied labels ({}) does not "
+                    "match the number of classes ({})"
+                ).format(len(labels), len(self.classes_))
+            )
 
         # Create the color mapping for the labels.
         self.color_values_ = resolve_colors(
-            n_colors=len(labels), colormap=self.colormap, colors=self.colors)
+            n_colors=len(labels), colormap=self.colormap, colors=self.colors
+        )
         colors = dict(zip(labels, self.color_values_))
 
         # Transform labels into a map of class to label
@@ -322,24 +321,23 @@ class UMAPVisualizer(TextVisualizer):
         # Expand the points into vectors of x and y for scatter plotting,
         # assigning them to their label if the label has been passed in.
         # Additionally, filter classes not specified directly by the user.
-        series = defaultdict(lambda: {'x':[], 'y':[]})
+        series = defaultdict(lambda: {"x": [], "y": []})
 
         if target is not None:
             for t, point in zip(target, points):
                 label = labels[t]
-                series[label]['x'].append(point[0])
-                series[label]['y'].append(point[1])
+                series[label]["x"].append(point[0])
+                series[label]["y"].append(point[1])
         else:
             label = self.classes_[0]
-            for x,y in points:
-                series[label]['x'].append(x)
-                series[label]['y'].append(y)
+            for x, y in points:
+                series[label]["x"].append(x)
+                series[label]["y"].append(y)
 
         # Plot the points
         for label, points in series.items():
             self.ax.scatter(
-                points['x'], points['y'], c=colors[label],
-                alpha=self.alpha, label=label
+                points["x"], points["y"], c=colors[label], alpha=self.alpha, label=label
             )
 
     def finalize(self, **kwargs):
@@ -347,9 +345,7 @@ class UMAPVisualizer(TextVisualizer):
         Finalize the drawing by adding a title and legend, and removing the
         axes objects that do not convey information about UMAP.
         """
-        self.set_title(
-            "UMAP Projection of {} Documents".format(self.n_instances_)
-        )
+        self.set_title("UMAP Projection of {} Documents".format(self.n_instances_))
 
         # Remove the ticks
         self.ax.set_yticks([])
@@ -360,6 +356,9 @@ class UMAPVisualizer(TextVisualizer):
             box = self.ax.get_position()
             self.ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
             manual_legend(
-                self, self.classes_, self.color_values_,
-                loc='center left', bbox_to_anchor=(1, 0.5)
+                self,
+                self.classes_,
+                self.color_values_,
+                loc="center left",
+                bbox_to_anchor=(1, 0.5),
             )
