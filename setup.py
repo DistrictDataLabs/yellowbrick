8838a6457aed58f5d3e31f63bbcbeb05601ab83b
pdamodaran <pdamodaran@users.noreply.github.com>
2019-08-28 19:32:35 -0400

    updated CLASSIFIERS list (#959)

--- setup.py
+++ setup.py
@@ -52,19 +52,18 @@ KEYWORDS = (
 
 ## Define the classifiers
 ## See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-CLASSIFIERS = (
-    "Development Status :: 4 - Beta",
+CLASSIFIERS  = (
+    "Development Status :: 5 - Production/Stable",
     "Intended Audience :: Developers",
     "Intended Audience :: Science/Research",
     "License :: OSI Approved :: Apache Software License",
     "Natural Language :: English",
     "Operating System :: OS Independent",
     "Programming Language :: Python",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
     "Topic :: Software Development",
     "Topic :: Software Development :: Libraries :: Python Modules",
     "Topic :: Scientific/Engineering :: Visualization",
