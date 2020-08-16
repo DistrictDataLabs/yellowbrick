bee1c3be9dd212ebbe7a749dc8829e61a82b2789
Tan Tran <tankimtran@gmail.com>
2020-08-16 11:47:02 -0400

    Add styles parameter to manual_legend
    Added styles parameter to manual_legend per issue #566. With tests and bug fix for existing manual_legend test.

--- tests/test_draw.py
+++ tests/test_draw.py
@@ -31,8 +31,30 @@ def test_manual_legend_uneven_colors():
     """
     Raise exception when colors and labels are mismatched in manual_legend
     """
-    with pytest.raises(YellowbrickValueError, match="same number of colors as labels"):
+    with pytest.raises(YellowbrickValueError, 
+        match="list of length equal to the number of labels"):
         manual_legend(None, ("a", "b", "c"), ("r", "g"))
+
+def test_manual_legend_styles_malformed_input():
+    """
+    Raise exception when styles and/or colors are not lists of same length
+    as labels
+    """
+
+    # styles should be a list of strings
+    with pytest.raises(YellowbrickValueError, 
+        match="Please specify the styles parameter as a list of strings"):
+        manual_legend(None, ("a", "b", "c"), styles="ro")
+
+    # styles should be a list of same len() as labels
+    with pytest.raises(YellowbrickValueError, 
+    match="list of length equal to the number of labels"):
+        manual_legend(None, ("a", "b", "c"), styles=("ro", "--"))
+
+    # if colors is passed in alongside styles, it should be of same length
+    with pytest.raises(YellowbrickValueError, 
+    match="list of length equal to the number of labels"):
+        manual_legend(None, ("a", "b", "c"), ("r", "g"), styles=("ro", "b--", "--"))
 
 
 @pytest.fixture(scope="class")
@@ -83,7 +105,45 @@ class TestDraw(VisualTestCase):
         )
 
         # Assert image similarity
-        self.assert_images_similar(ax=ax, tol=0.5)
+        self.assert_images_similar(ax=ax, tol=0.5, remove_legend=False)
+
+    def test_manual_legend_styles(self):
+        """
+        Check that the styles argument to manual_legend is correctly
+        processed, including its being overridden by the colors argument
+        """
+        
+        # Draw a random scatter plot
+        random = np.random.RandomState(42)
+
+        Ax, Ay = random.normal(50, 2, 100), random.normal(50, 3, 100)
+        Bx, By = random.normal(42, 3, 100), random.normal(44, 1, 100)
+        Cx, Cy = random.normal(20, 10, 100), random.normal(30, 1, 100)
+        Dx, Dy = random.normal(33, 5, 100), random.normal(22, 2, 100)
+
+        _, ax = plt.subplots()
+        ax.scatter(Ax, Ay, c="r", alpha=0.35, label="a")
+        ax.scatter(Bx, By, c="g", alpha=0.35, label="b")
+        ax.scatter(Cx, Cy, c="b", alpha=0.35, label="c")
+        ax.scatter(Dx, Dy, c="y", alpha=0.35, label="d")
+
+        # Four style/color combinations are tested here:
+        # (1) "blue" color should override the "r" of "ro" style
+        # (2) blank color should, of course, be overriden by the "g" of "-g"
+        # (3) None color should also be overridden by the third style, but 
+        #   since a color is not specified there either, the entry should 
+        #   default to black.
+        # (4) Linestyle, marker, and color are all unspecified. The entry should
+        #   default to a solid black line.
+        styles = ["ro", "-g", "--", ""]
+        labels = ("a", "b", "c", "d")
+        colors = ("blue", "", None, None)
+        manual_legend(
+            ax, labels, colors, styles=styles, frameon=True, loc="upper left"
+        )
+
+        # Assert image similarity
+        self.assert_images_similar(ax=ax, tol=0.5, remove_legend=False)
 
     def test_vertical_bar_stack(self):
         """
