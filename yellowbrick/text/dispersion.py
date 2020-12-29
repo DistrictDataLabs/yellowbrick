babb521fffa0c73d3a99972a651bbc6b887d519c
Rebecca Bilbro <rebeccabilbro@users.noreply.github.com>
2020-12-29 11:28:56 -0500

    Update dispersion plot to allow title and color customization (#1136)
    * update dispersion plot to allow title and color customization
    
    * small updates to dispersion tests
    
    * avoid sklearn 0.24 since it has build issues with some OSes
    
    * skip tests if no pandas

--- yellowbrick/text/dispersion.py
+++ yellowbrick/text/dispersion.py
@@ -18,14 +18,15 @@ Implementation of lexical dispersion for text visualization
 ## Imports
 ##########################################################################
 
-from collections import defaultdict
 import itertools
+from collections import defaultdict
+
+import numpy as np
 
 from yellowbrick.text.base import TextVisualizer
 from yellowbrick.style.colors import resolve_colors
 from yellowbrick.exceptions import YellowbrickValueError
 
-import numpy as np
 
 ##########################################################################
 ## Dispersion Plot Visualizer
@@ -34,31 +35,39 @@ import numpy as np
 
 class DispersionPlot(TextVisualizer):
     """
-    DispersionPlotVisualizer allows for visualization of the lexical dispersion
-    of words in a corpus.  Lexical dispersion is a measure of a word's
-    homeogeneity across the parts of a corpus.  This plot notes the occurences
-    of a word and how many words from the beginning it appears.
+    Lexical dispersion illustrates the homogeneity of a word (or set of words) across
+    the documents of a corpus.
+
+    DispersionPlot allows for visualization of the lexical dispersion of words in a
+    corpus. This plot illustrates with vertical lines the occurrences of one or more
+    search terms throughout the corpus, noting how many words relative to the beginning
+    of the corpus it appears. If the target vector of the corpus documents is provided,
+    the points will be colored with respect to their document category, which allows for
+    additional analysis of relationships in search term homogeneity within and across
+    document categories. If annotation is requested, document boundaries will be
+    displayed as vertical lines in the plot.
 
     Parameters
     ----------
-    target_words : list
-        A list of target words whose dispersion across a corpus passed at fit
-        will be visualized.
+    search_terms : list
+        A list of search terms whose dispersion across a corpus passed at fit
+        should be visualized.
 
     ax : matplotlib axes, default: None
         The axes to plot the figure on.
 
     colors : list or tuple of colors
-        Specify the colors for each individual class
+        Specify the colors for each individual class. Will override colormap if both are
+        provided.
 
     colormap : string or matplotlib cmap
         Qualitative colormap for discrete target
 
     ignore_case : boolean, default: False
-        Specify whether input  will be case-sensitive.
+        Specify whether input will be case-sensitive.
 
     annotate_docs : boolean, default: False
-        Specify whether document boundaries will be displayed.  Vertical lines
+        Specify whether document boundaries will be displayed. Vertical lines
         are positioned at the end of each document.
 
     labels : list of strings
@@ -68,8 +77,24 @@ class DispersionPlot(TextVisualizer):
     kwargs : dict
         Pass any additional keyword arguments to the super class.
 
-    These parameters can be influenced later on in the visualization
-    process, but can and should be set as early as possible.
+    Attributes
+    ----------
+    self.classes_ : list
+        A list of strings representing the unique classes in the target in sorted order.
+        If ``y`` is provided, these are extracted from ``y``, unless a list of class
+        labels is provided by the user on instantiation.
+
+    self.boundaries_ : list
+        A list of integers indicating the document boundaries with respect to
+        word offsets.
+
+    self.indexed_words_ : list
+        A list of integers indicating the y position for each occurrence of each of
+        the search terms.
+
+    self.word_categories_ : list
+        A list of strings indicating the corresponding document category of each search
+        term occurrence.
     """
 
     # NOTE: cannot be np.nan
@@ -77,7 +102,7 @@ class DispersionPlot(TextVisualizer):
 
     def __init__(
         self,
-        target_words,
+        search_terms,
         ax=None,
         colors=None,
         colormap=None,
@@ -92,18 +117,28 @@ class DispersionPlot(TextVisualizer):
         self.colors = colors
         self.colormap = colormap
 
-        self.target_words = target_words
         self.ignore_case = ignore_case
+        self.search_terms = search_terms
         self.annotate_docs = annotate_docs
 
-    def _compute_dispersion(self, text, y):
+    def _compute_dispersion(self, X, y):
+        """
+        Produces a generator containing the offset word count, y_coordinate, and
+        label/category for each occurrance of the search terms.
+
+        Attributes
+        ----------
+        self.boundaries_ : list
+            A list of integers indicating the document boundaries with respect to
+            word offsets.
+        """
         self.boundaries_ = []
         offset = 0
 
         if y is None:
             y = itertools.repeat(None)
 
-        for doc, target in zip(text, y):
+        for doc, category in zip(X, y):
             for word in doc:
                 if self.ignore_case:
                     word = word.lower()
@@ -114,18 +149,24 @@ class DispersionPlot(TextVisualizer):
                 offset += 1
                 for y_coord in (self.indexed_words_ == word).nonzero()[0]:
                     y_coord = int(y_coord)
-                    yield (offset, y_coord, target)
+                    yield (offset, y_coord, category)
+
             if self.annotate_docs:
                 self.boundaries_.append(offset)
+
         self.boundaries_ = np.array(self.boundaries_, dtype=int)
 
     def _check_missing_words(self, points):
+        """
+        Helper method to raise an error if any of the requested search
+        terms do not appear in the corpus.
+        """
         for index in range(len(self.indexed_words_)):
             if index in points[:, 1]:
                 pass
             else:
                 raise YellowbrickValueError(
-                    ("The indexed word '{}' is not found in " "this corpus").format(
+                    ("The search term '{}' is not found in " "this corpus").format(
                         self.indexed_words_[index]
                     )
                 )
@@ -147,47 +188,62 @@ class DispersionPlot(TextVisualizer):
             instances. If this is specified, then the points will be colored
             according to their class.
 
-        kwargs : dict
-            Pass generic arguments to the drawing method
-
         Returns
         -------
         self : instance
             Returns the instance of the transformer/visualizer
+
+        Attributes
+        ----------
+        self.classes_ : list
+            A list of strings representing the unique classes in the target in sorted order.
+            If ``y`` is provided, these are extracted from ``y``, unless a list of class
+            labels is provided by the user on instantiation.
+
+        self.indexed_words_ : list
+            A list of integers indicating the y position for each occurrence of each of
+            the search terms.
+
+        self.word_categories_ : list
+            A list of strings indicating the corresponding document category of each search
+            term occurrence.
         """
 
         if y is not None:
             self.classes_ = np.unique(y)
-        elif y is None and self.labels is not None:
-            self.classes_ = np.array([self.labels[0]])
         else:
             self.classes_ = np.array([self.NULL_CLASS])
 
         # Create an index (e.g. the y position) for the target words
-        self.indexed_words_ = np.flip(self.target_words, axis=0)
+        self.indexed_words_ = np.flip(self.search_terms, axis=0)
         if self.ignore_case:
             self.indexed_words_ = np.array([w.lower() for w in self.indexed_words_])
 
         # Stack is used to create a 2D array from the generator
         try:
-            points_target = np.stack(self._compute_dispersion(X, y))
+            offsets_positions_categories = np.stack(self._compute_dispersion(X, y))
         except ValueError:
-            raise YellowbrickValueError(("No indexed words were found in the corpus"))
-        points = np.stack(
-            zip(points_target[:, 0].astype(int), points_target[:, 1].astype(int))
+            raise YellowbrickValueError(("No search terms were found in the corpus"))
+
+        word_positions = np.stack(
+            zip(
+                offsets_positions_categories[:, 0].astype(int),
+                offsets_positions_categories[:, 1].astype(int),
+            )
         )
 
-        self.target = points_target[:, 2]
+        self.word_categories_ = offsets_positions_categories[:, 2]
 
-        self._check_missing_words(points)
+        self._check_missing_words(word_positions)
 
-        self.draw(points, self.target)
+        self.draw(word_positions, **kwargs)
         return self
 
-    def draw(self, points, target=None, **kwargs):
+    def draw(self, points, **kwargs):
         """
         Called from the fit method, this method creates the canvas and
         draws the plot on it.
+
         Parameters
         ----------
         kwargs: generic keyword arguments.
@@ -205,7 +261,7 @@ class DispersionPlot(TextVisualizer):
 
         # Create the color mapping for the labels.
         color_values = resolve_colors(
-            n_colors=len(labels), colormap=self.colormap, colors=self.color
+            n_colors=len(labels), colormap=self.colormap, colors=self.colors
         )
         colors = dict(zip(labels, color_values))
 
@@ -219,8 +275,8 @@ class DispersionPlot(TextVisualizer):
 
         series = defaultdict(lambda: {"x": [], "y": []})
 
-        if target is not None:
-            for point, t in zip(points, target):
+        if self.word_categories_ is not None:
+            for point, t in zip(points, self.word_categories_):
                 label = labels[t]
                 series[label]["x"].append(point[0])
                 series[label]["y"].append(point[1])
@@ -258,9 +314,8 @@ class DispersionPlot(TextVisualizer):
         -----
         Generally this method is called from show and not directly by the user.
         """
-
+        self.set_title("Lexical Dispersion Plot")
         self.ax.set_ylim(-1, len(self.indexed_words_))
-        self.ax.set_title("Lexical Dispersion Plot")
         self.ax.set_xlabel("Word Offset")
         self.ax.grid(False)
 
@@ -275,8 +330,9 @@ class DispersionPlot(TextVisualizer):
 ## Quick Method
 ##########################################################################
 
+
 def dispersion(
-    target_words,
+    search_terms,
     corpus,
     y=None,
     ax=None,
@@ -296,7 +352,7 @@ def dispersion(
     Parameters
     ----------
 
-    target_words : list
+    search_terms : list
         A list of words whose dispersion will be examined within a corpus
 
     corpus : list
@@ -312,7 +368,8 @@ def dispersion(
         The axes to plot the figure on.
 
     colors : list or tuple of colors
-        Specify the colors for each individual class
+        Specify the colors for each individual class. Will override colormap if both are
+        provided.
 
     colormap : string or matplotlib cmap
         Qualitative colormap for discrete target
@@ -344,7 +401,7 @@ def dispersion(
 
     # Instantiate the visualizer
     visualizer = DispersionPlot(
-        target_words,
+        search_terms,
         ax=ax,
         colors=colors,
         colormap=colormap,
