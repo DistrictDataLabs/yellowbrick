ad81093282a7b256b8d3b64cf52f5f8275340ded
Rebecca Bilbro <bilbro@gmail.com>
2021-02-09 09:50:22 -0500

    version bump:

--- yellowbrick/version.py
+++ yellowbrick/version.py
@@ -19,11 +19,11 @@ Maintains version and package information for deployment.
 
 __version_info__ = {
     "major": 1,
-    "minor": 2,
-    "micro": 1,
+    "minor": 3,
+    "micro": 0,
     "releaselevel": "final",
     "post": 0,
-    "serial": 19,
+    "serial": 20,
 }
 
 ##########################################################################
