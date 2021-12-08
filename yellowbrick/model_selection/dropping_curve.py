1834ec3acf8d1f888d198630fe0c7207713b76f3
charles <charlesguan94@gmail.com>
2021-12-08 15:04:16 -0800

    Create dropping_curve.py

---
+++ yellowbrick/model_selection/dropping_curve.py
@@ -0,0 +1,5 @@
+# yellowbrick.model_selection.dropping_curve
+# Implements a input-feature dropping curve visualization for model selection.
+#
+# Author:   Charles Guan
+# Created:  Wed Dec 8 15:03:00 2021 -0800
